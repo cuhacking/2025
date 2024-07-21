275b4645e8ffc7af66975dd217940abf87c4affd
Jowi <83415433+JowiAoun@users.noreply.github.com>
2024-07-21 09:03:04 -0400

    Delete example post API & usages to fix production build errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,17 @@
 import Link from "next/link";
 
-import { LatestPost } from "~/app/_components/post";
-import { getServerAuthSession } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
+import { getServerAuthSession } from "~/server/auth";
 import { UserProfile } from "./_components/userProfile/UserProfile";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
   const session = await getServerAuthSession();
-
-  void api.post.getLatest.prefetch();
 
   if (!session) {
     return (
       <div>
         <p>
-          Welcome to <code>trpc</code> with <code>next-auth</code>!
-        </p>
-        <p>
-          <Link href="/api/auth/signin">Sign in</Link> to see the latest post.
+          <Link href="/api/auth/signin">Sign in</Link>
         </p>
       </div>
     );
@@ -26,7 +19,7 @@ export default async function Home() {
 
   const team = await api.team.getByUserId({userId: session.user.id})
 
-  if (!team) return <h1>Error: no team found for user</h1>
+  if (!team) return <h1>Error: No team found for user</h1>
 
   return (
     <HydrateClient>
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
-import { postRouter } from "~/server/api/routers/post";
 import { teamRouter } from "~/server/api/routers/team";
 
 /**
@@ -8,7 +7,6 @@ import { teamRouter } from "~/server/api/routers/team";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   team: teamRouter,
 });
 
@@ -19,7 +17,7 @@ export type AppRouter = typeof appRouter;
  * Create a server-side caller for the tRPC API.
  * @example
  * const trpc = createCaller(createContext);
- * const res = await trpc.post.all();
- *       ^? Post[]
+ * const res = await trpc.team.getByUserId(userId);
+ *       ^? Team
  */
 export const createCaller = createCallerFactory(appRouter);
