fb280475eb477f3980614cbac7a7b7ce77794343
Jowi <83415433+JowiAoun@users.noreply.github.com>
2024-07-21 23:21:42 -0400

    Divide procedures

---
+++ src/server/api/routers/team/constants.ts
@@ -0,0 +1,11 @@
+import {z} from "zod";
+
+
+export const joinUserTeamSchema = z.object({
+  userId: z.string().min(1),
+  teamId: z.string().min(1)
+})
+
+export const leaveUserTeamSchema = z.object({
+  userId: z.string().min(1)
+})
