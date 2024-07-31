4e11c1c839f619fb9c035ae6e9857d61b11caea7
HasithDeAlwis <hasithde24@gmail.com>
2024-07-31 19:56:12 -0400

    removed more instances of .post model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import Link from "next/link";
 
-import { LatestPost } from "~/app/_components/post";
 import { getServerAuthSession } from "~/server/auth";
-import { api, HydrateClient } from "~/trpc/server";
+import { HydrateClient } from "~/trpc/server";
 import { UserProfile } from "./_components/userProfile/UserProfile";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
   const session = await getServerAuthSession();
-
-  void api.post.getLatest.prefetch();
 
   if (!session) {
     return (
