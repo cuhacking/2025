cb5dd219291f410dab43d3c61577756a1d594905
Mumtahin Farabi <mfarabi619@gmail.com>
2024-08-03 23:56:28 -0400

    chore(repo): add playwright eslint rules to config

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import antfu from '@antfu/eslint-config'
+import playwright from 'eslint-plugin-playwright'
+
+export default antfu(
+  {
+    formatters: true,
+    react: true,
+  },
+  {
+    ...playwright.configs['flat/recommended'],
+  },
+)
