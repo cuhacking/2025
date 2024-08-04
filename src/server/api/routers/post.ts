c85a0f1d209f4ff978dd633e55795ed8a0319e87
Clover <muktarakinbile@cmail.carleton.ca>
2024-08-04 01:08:11 -0400

    Application form feature

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -39,4 +39,39 @@ export const postRouter = createTRPCRouter({
   getSecretMessage: protectedProcedure.query(() => {
     return 'you can now see this secret message!'
   }),
+
+  submitApplicationForm: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        email: z.string().email().nullable(),
+        first_name: z.string().nullable(),
+        last_name: z.string().nullable(),
+        levels_of_study: z.string().nullable(), 
+        major: z.string().nullable(), 
+        date_of_birth: z.date().nullable(), 
+        gender: z.string().nullable(), 
+        phone_number: z.string().nullable(),  
+        school: z.string().nullable(),        
+        userId: z.string(),       
+    })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const data = await ctx.db.userInformation.create({
+        data:{
+          id: input.id,
+          email: input.email,
+          first_name: input.first_name,
+          last_name: input.last_name,
+          levels_of_study: input.levels_of_study,
+          major: input.major,
+          date_of_birth: input.date_of_birth,
+          gender: input.gender,
+          phone_number: input.phone_number,
+          school: input.school,
+          userId: input.userId
+        },
+      });
+      return data;
+    }),
 })
