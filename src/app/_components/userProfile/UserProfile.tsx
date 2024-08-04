6f426b17879a9bf6823a37952334fe1cbb474ea8
Jowi <83415433+JowiAoun@users.noreply.github.com>
2024-08-04 00:00:49 -0400

    Use camelCase, move logic inside of component

--- src/app/_components/userProfile/UserProfile.tsx
+++ src/app/_components/userProfile/UserProfile.tsx
@@ -1,36 +1,49 @@
 import QRCode from 'react-qr-code'
-import type { Session } from 'next-auth'
-import type { Team } from '@prisma/client'
+import { TRPCError } from '@trpc/server'
 import SignOut from '~/app/_components/userProfile/signOut'
+import { api } from '~/trpc/server'
+import { getServerAuthSession } from '~/server/auth'
 
-export default function UserProfile({
-  SESSION,
-  TEAM,
-}: {
-  SESSION: Session
-  TEAM: Team
-}) {
+export default async function UserProfile() {
+  const session = await getServerAuthSession()
+
+  if (!session) {
+    throw new TRPCError({
+      code: 'NOT_FOUND',
+      message: `Session not found`,
+    })
+  }
+
+  const team = await api.team.getByUserId({ userId: session.user.id })
+
+  if (!team) {
+    throw new TRPCError({
+      code: 'NOT_FOUND',
+      message: `Team not found`,
+    })
+  }
+
   return (
     <div>
       <h1>Your application is just your profile.</h1>
       <h2>All you have to do is fill it in.</h2>
-      {JSON.stringify(SESSION, null, 2)}
+      {JSON.stringify(session, null, 2)}
 
       <h1>Your team</h1>
       <p>
         Team name:
-        {TEAM.name}
+        {team.name}
       </p>
       <p>
         Invite code:
-        {TEAM.id}
+        {team.id}
       </p>
 
       <p>Share this QR code with your friends to invite them!</p>
       <QRCode
         size={256}
         style={{ height: 'auto', maxWidth: '15%', width: '15%', margin: '2rem' }}
-        value={`http://localhost:8000/t/j/${TEAM.id}`}
+        value={`http://localhost:8000/t/j/${team.id}`}
         viewBox="0 0 256 256"
       />
 
