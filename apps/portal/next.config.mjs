43f8210cde729f8641da31524d5984e43f1aa8f5
Mumtahin Farabi <mfarabi619@gmail.com>
2024-08-13 12:33:13 -0400

    build(portal): migrate next config to ESM

--- apps/portal/next.config.mjs
+++ apps/portal/next.config.mjs
@@ -1,6 +1,4 @@
-// @ts-check
-
-const { composePlugins, withNx } = require('@nx/next')
+import { composePlugins, withNx } from '@nx/next'
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -18,4 +16,4 @@ const plugins = [
   withNx,
 ]
 
-module.exports = composePlugins(...plugins)(nextConfig)
+export default composePlugins(...plugins)(nextConfig)
