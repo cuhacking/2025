30d79666992e6d23eeea99f49f2881186831c98a
Mumtahin Farabi <mfarabi619@gmail.com>
2024-08-13 11:24:06 -0400

    chore(config/repo/eslint): migrate to use flat config and anthony fu's rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,44 +1,55 @@
-const { FlatCompat } = require('@eslint/eslintrc');
-const nxEslintPlugin = require('@nx/eslint-plugin');
-const js = require('@eslint/js');
+// const { FlatCompat } = require('@eslint/eslintrc');
+// const nxEslintPlugin = require('@nx/eslint-plugin');
+// const js = require('@eslint/js');
+//
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+//   recommendedConfig: js.configs.recommended,
+// });
+//
+// module.exports = [
+//   { plugins: { '@nx': nxEslintPlugin } },
+//   {
+//     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+//     rules: {
+//       '@nx/enforce-module-boundaries': [
+//         'error',
+//         {
+//           enforceBuildableLibDependency: true,
+//           allow: [],
+//           depConstraints: [
+//             {
+//               sourceTag: '*',
+//               onlyDependOnLibsWithTags: ['*'],
+//             },
+//           ],
+//         },
+//       ],
+//       // TODO: get these nx eslint rules working
+//       // '@nx/nx-plugin-checks': ['warning'],
+//       // '@nx/nx-dependency-checks': ['warning'],
+//     },
+//   },
+//   ...compat.config({ extends: ['plugin:@nx/typescript'] }).map((config) => ({
+//     ...config,
+//     files: ['**/*.ts', '**/*.tsx'],
+//     rules: {
+//       ...config.rules,
+//     },
+//   })),
+//   ...compat.config({ extends: ['plugin:@nx/javascript'] }).map((config) => ({
+//     ...config,
+//     files: ['**/*.js', '**/*.jsx'],
+//     rules: {
+//       ...config.rules,
+//     },
+//   })),
+// ];
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-});
+const antfu = require('@antfu/eslint-config').default
 
-module.exports = [
-  { plugins: { '@nx': nxEslintPlugin } },
-  {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-    rules: {
-      '@nx/enforce-module-boundaries': [
-        'error',
-        {
-          enforceBuildableLibDependency: true,
-          allow: [],
-          depConstraints: [
-            {
-              sourceTag: '*',
-              onlyDependOnLibsWithTags: ['*'],
-            },
-          ],
-        },
-      ],
-    },
-  },
-  ...compat.config({ extends: ['plugin:@nx/typescript'] }).map((config) => ({
-    ...config,
-    files: ['**/*.ts', '**/*.tsx'],
-    rules: {
-      ...config.rules,
-    },
-  })),
-  ...compat.config({ extends: ['plugin:@nx/javascript'] }).map((config) => ({
-    ...config,
-    files: ['**/*.js', '**/*.jsx'],
-    rules: {
-      ...config.rules,
-    },
-  })),
-];
+module.exports = antfu({
+  formatters: true,
+  react: true,
+})
+
