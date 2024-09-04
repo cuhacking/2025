cbd56f98b18a06fd9ab9ebe27f3d89b350656fb4
JeremyFriesenGitHub <jeremyfriesen12@gmail.com>
2024-09-04 10:26:28 -0400

    style(docs-e2e): added comments for test

--- apps/docs-e2e/src/example.spec.ts
+++ apps/docs-e2e/src/example.spec.ts
@@ -4,5 +4,6 @@ test('has title', async ({ page }) => {
   await page.goto('/')
 
   // Expect h1 to contain a substring.
+  // See https://playwright.dev/docs/api/class-locator#locator-textcontent
   expect(await page.locator('h1').textContent()).toContain('Welcome')
 })
