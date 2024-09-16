1ebb188b892b2fdd1d31b5772e09fd1417135de4
jowi <jowiaoun@hotmail.com>
2024-09-15 22:14:34 -0400

    feat(api/db): create sample database schema

--- libs/db/src/index.ts
+++ libs/db/src/index.ts
@@ -1,1 +1,14 @@
-export * from './lib/db'
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+import { envWebsiteDb } from '@cuhacking/env'
+import * as schema from './schema'
+
+const sql = postgres(envWebsiteDb.DATABASE_URL)
+
+export const db = drizzle(sql, {
+  schema,
+  logger: envWebsiteDb.NODE_ENV === 'development',
+})
+
+export * from './schema/user'
+export * from './schema/session'
---
+++ libs/db/src/schema/session.ts
@@ -0,0 +1,14 @@
+import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+
+import { user } from './user'
+
+export const session = pgTable('session', {
+  id: text('id').primaryKey(),
+  userId: text('user_id')
+    .notNull()
+    .references(() => user.id),
+  expiresAt: timestamp('expires_at', {
+    withTimezone: true,
+    mode: 'date',
+  }).notNull(),
+})
