9f5c189d5bd30d552c1f70fb1f56902ed928ac27
jowi <jowiaoun@hotmail.com>
2024-09-16 22:27:25 -0400

    feat(api/api): create initial user route

--- libs/api/src/index.ts
+++ libs/api/src/index.ts
@@ -1,1 +1,14 @@
+import { userRouter } from './routes/user'
+import { createCallerFactory, createRouter } from './trpc'
+
 export * from './lib/api'
+
+export const appRouter = createRouter({
+  user: userRouter,
+})
+
+export type AppRouter = typeof appRouter
+
+export const createCaller = createCallerFactory(appRouter)
+
+export { createTRPCContext } from './trpc'
