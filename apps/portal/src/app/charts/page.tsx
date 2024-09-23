1cddfcf29d6ae8daa5806cdcbc7d730e29f552d8
JeremyFriesenGitHub <jeremyfriesen12@gmail.com>
2024-09-22 21:57:13 -0400

    feat(portal/charts): create portal charts page

---
+++ apps/portal/src/app/charts/page.tsx
@@ -0,0 +1,21 @@
+export default function Charts() {
+  /*
+     * Replace the elements below with your own.
+     *
+     * Note: The corresponding styles are in the ./index.tailwind file.
+     */
+  return (
+    <div>
+      <div className="wrapper">
+        <div className="container">
+          <div id="welcome">
+            <h1>
+              <span> Hello there, </span>
+              Welcome to Charts 👋
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
