6096dea24009988bcc6447232abb08301ec6af28
JeremyFriesenGitHub <jeremyfriesen12@gmail.com>
2024-09-22 21:56:48 -0400

    feat(portal/charts): create charts layout file

---
+++ apps/portal/src/app/charts/layout.tsx
@@ -0,0 +1,17 @@
+import '../global.css'
+
+export const metadata = {
+  title: 'Welcome to portal charts',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
