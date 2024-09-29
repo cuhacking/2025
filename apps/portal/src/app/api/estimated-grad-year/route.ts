3579f6f688b7c85bceddd06cc921296bb70e9e83
JeremyFriesenGitHub <jeremyfriesen12@gmail.com>
2024-09-29 03:27:05 -0400

    feat(portal): add routes

---
+++ apps/portal/src/app/api/estimated-grad-year/route.ts
@@ -0,0 +1,47 @@
+import process from 'node:process'
+import { NextResponse } from 'next/server'
+import { Pool } from 'pg'
+
+const pool = new Pool({
+  user: process.env.DB_USER || 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'project_database',
+  password: process.env.DB_PASSWORD || '1234',
+  port: Number(process.env.DB_PORT) || 5432,
+})
+
+export async function GET() {
+  try {
+    const client = await pool.connect()
+
+    if (!client) {
+      return NextResponse.json({ error: 'Database connection failed' }, { status: 500 })
+    }
+
+    const query = `
+        SELECT estimated_grad_year, COUNT(*) as count
+        FROM "User"
+        GROUP BY estimated_grad_year
+        ORDER BY estimated_grad_year ASC;
+    `
+    const result = await client.query(query)
+
+    if (result.rows.length === 0) {
+      return NextResponse.json({ data: [] })
+    }
+
+    client.release()
+
+    return NextResponse.json({ data: result.rows })
+  }
+  catch (err: any) {
+    console.error('Error fetching data from PostgreSQL', err)
+
+    if (err.message.includes('database connection')) {
+      return NextResponse.json({ error: 'Database connection failed' }, { status: 500 })
+    }
+    else {
+      return NextResponse.json({ error: 'Server error' }, { status: 500 })
+    }
+  }
+}
