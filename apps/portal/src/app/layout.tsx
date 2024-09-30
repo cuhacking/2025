e8824608f2d9d7ae98a2761d2ba084b0255c6d01
JeremyFriesenGitHub <jeremyfriesen12@gmail.com>
2024-09-29 21:20:53 -0400

    feat(portal): update layout to include theme toggle

--- apps/portal/src/app/layout.tsx
+++ apps/portal/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './global.css'
+import { ThemeProvider } from '../components/ui/theme-provider/theme-provider'
 
 export const metadata = {
   title: 'Welcome to portal',
@@ -11,8 +12,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
