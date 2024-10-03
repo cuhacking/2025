beeda9dbf81feb4667e0e0d8f1f2d87c5de9d78b
Mumtahin Farabi <mfarabi619@gmail.com>
2024-10-03 19:16:09 -0400

    fix(libs/db): temporary workaround to get migration command working

--- libs/db/drizzle.config.ts
+++ libs/db/drizzle.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'drizzle-kit'
 
-import { envWebsiteDb } from '@cuhacking/env'
+// import { envWebsiteDb } from "@cuhacking/env";
 
 export default defineConfig({
   dialect: 'postgresql',
   schema: './libs/db/src/schema/index.ts',
   out: './drizzle',
   dbCredentials: {
-    url: envWebsiteDb.DATABASE_URL,
+    url: `postgresql://postgres:password@localhost:5432/hackathon`,
   },
 })
