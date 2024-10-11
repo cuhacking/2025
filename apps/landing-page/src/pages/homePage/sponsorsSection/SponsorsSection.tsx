aae0c1db507ae6f03f608a296f1e74b573d1e792
HasithDeAlwis <hasithde24@gmail.com>
2024-10-11 19:13:45 -0400

    chore(landing-page/sponsor-section): remove current sponsor and sponsor sign up

--- apps/landing-page/src/pages/homePage/sponsorsSection/SponsorsSection.tsx
+++ apps/landing-page/src/pages/homePage/sponsorsSection/SponsorsSection.tsx
@@ -1,22 +1,11 @@
-import { StandoutStickersLogo } from "../../../components/MLHBadge";
 import { Heading } from "../../../components/Typography";
-import { SponsorEmailSignup } from "../heroSection/EmailSignup";
 import { PastSponsors } from "./PastSponsors";
 
 export const SponsorsSection = () => {
   return (
-    <>
-      <div id="current-sponsors">
-        <Heading>Current Sponsors</Heading>
-        <div className="flex justify-center items-center">
-          <StandoutStickersLogo />
-        </div>
-      </div>
-      <div id="sponsors">
+      <div id="sponsors" className="mb-16 lg:mt-36">
         <Heading>Past Sponsors</Heading>
         <PastSponsors />
-        <SponsorEmailSignup className="w-5/6 md:w-1/2 mx-auto my-20 max-w-lg" />
       </div>
-    </>
   );
 };
