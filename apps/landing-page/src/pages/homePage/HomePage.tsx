645300469ede950b2e2526070e6e16d94b5dc21c
HasithDeAlwis <hasithde24@gmail.com>
2024-10-11 19:10:32 -0400

    refactor(landing-page/home): remove FAQ section

--- apps/landing-page/src/pages/homePage/HomePage.tsx
+++ apps/landing-page/src/pages/homePage/HomePage.tsx
@@ -1,19 +1,16 @@
 import "minireset.css";
 import "./HomePage.css";
 import { Hero } from "./heroSection/Hero";
-import { ConfettiProvider } from "./heroSection/Confetti";
 // import { AboutSection } from "./aboutSection/AboutSection";
 import { SponsorsSection } from "./sponsorsSection/SponsorsSection";
-import { FaqSection } from "./faqSection";
 
 export const App = () => {
   return (
-    <ConfettiProvider>
-      <Hero />
-      {/* <AboutSection /> */} {/* We are temporarily removing this section */}
-      <FaqSection />
-      <SponsorsSection />
-    </ConfettiProvider>
+    <>
+     <Hero />
+     <SponsorsSection />
+    </>
+     
   );
 };
 
