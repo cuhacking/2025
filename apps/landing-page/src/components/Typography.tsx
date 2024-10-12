dbab99d743f9bb5e7f513a2331436ebf72e7e0ad
HasithDeAlwis <hasithde24@gmail.com>
2024-10-11 20:42:38 -0400

    fix(landing-page): remove broken link to headers

--- apps/landing-page/src/components/Typography.tsx
+++ apps/landing-page/src/components/Typography.tsx
@@ -31,8 +31,8 @@ export const Text: React.FC<{ children: React.ReactNode; typo?: Typo; className?
 
 export const Heading = (props: { children: React.ReactNode }) => {
 	return (
-		<div className="my-14 mt-32 px-20 ">
-			<a className="flex justify-center items-center" href="heading">
+		<div className="mt-32 my-14">
+			<a className="flex items-center justify-center px-12">
 				<Text typo={Typo.TITLE_2}>❆*</Text>
 				<Text typo={Typo.TITLE_2} className="mx-10 font-bold text-center ">{props.children}</Text>
 				<Text typo={Typo.TITLE_2}>*❆</Text>
