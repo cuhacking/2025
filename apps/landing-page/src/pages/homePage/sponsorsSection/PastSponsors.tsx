5247684683a3cdf25fec60ad9b0d1e7880e2c92c
HasithDeAlwis <hasithde24@gmail.com>
2024-10-11 20:57:16 -0400

    feat(landing-page/hero): replace email sign up forms with icons

--- apps/landing-page/src/pages/homePage/sponsorsSection/PastSponsors.tsx
+++ apps/landing-page/src/pages/homePage/sponsorsSection/PastSponsors.tsx
@@ -6,7 +6,7 @@ export const PastSponsors = () => {
       {PAST_SPONSOR_IMAGES.map((sponsor, index) => (
         <a href={sponsor.url} target="_blank" rel="noopener noreferrer" key={index}>
           <img
-            className="w-auto transition-transform duration-500 md:max-h-24 hover:scale-110"
+            className="w-auto transition-transform duration-500 max-h-24 hover:scale-110"
             src={sponsor.imgSrc}
             alt="Sponsor logo"
           />
