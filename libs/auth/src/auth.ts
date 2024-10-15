2ebaa986f75e714293d5d879b45e7798836759ec
jowi <jowiaoun@hotmail.com>
2024-10-15 00:17:53 -0400

    fix(build/web): remove cache & cookies

--- libs/auth/src/auth.ts
+++ libs/auth/src/auth.ts
@@ -1,5 +1,7 @@
-import { cache } from 'react'
-import { cookies } from 'next/headers'
+// TODO: Implement cache & cookies back, attempt without using next - website should build successfully
+
+// import { cache } from 'react'
+// import { cookies } from 'next/headers'
 
 import type { Session, User } from 'lucia'
 
@@ -8,27 +10,28 @@ import { lucia } from './lucia'
 export async function uncachedAuth(): Promise<
   { user: User, session: Session } | { user: null, session: null }
 > {
-  const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null
+  // const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null
+  const sessionId = null
   if (!sessionId) {
     return { user: null, session: null }
   }
   const result = await lucia.validateSession(sessionId)
   try {
     if (result.session?.fresh) {
-      const sessionCookie = lucia.createSessionCookie(result.session.id)
-      cookies().set(
-        sessionCookie.name,
-        sessionCookie.value,
-        sessionCookie.attributes,
-      )
+      // const sessionCookie = lucia.createSessionCookie(result.session.id)
+      // cookies().set(
+      //   sessionCookie.name,
+      //   sessionCookie.value,
+      //   sessionCookie.attributes,
+      // )
     }
     if (!result.session) {
-      const sessionCookie = lucia.createBlankSessionCookie()
-      cookies().set(
-        sessionCookie.name,
-        sessionCookie.value,
-        sessionCookie.attributes,
-      )
+      // const sessionCookie = lucia.createBlankSessionCookie()
+      // cookies().set(
+      //   sessionCookie.name,
+      //   sessionCookie.value,
+      //   sessionCookie.attributes,
+      // )
     }
   }
   catch {
@@ -37,4 +40,5 @@ export async function uncachedAuth(): Promise<
   return result
 }
 
-export const auth = cache(uncachedAuth)
+// export const auth = cache(uncachedAuth)
+export const auth = uncachedAuth
