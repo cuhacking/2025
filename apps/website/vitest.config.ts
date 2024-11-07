47c20fd9ae7c114ff83b8d06604f419229ae7bef
Mumtahin Farabi <mfarabi619@gmail.com>
2024-11-06 23:10:22 -0500

    feat(website): scaffold app

---
+++ apps/website/vitest.config.ts
@@ -0,0 +1,27 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/apps/website',
+  plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+  test: {
+    setupFiles: ['test-setup.ts'],
+    watch: false,
+    globals: true,
+    environment: 'jsdom',
+    include: ['./tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../../coverage/apps/website',
+      provider: 'v8',
+    },
+  },
+});
