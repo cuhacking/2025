c38beebb9bdb54ff80635cf2e1eb3ce434695d3f
Mumtahin Farabi <mfarabi619@gmail.com>
2024-11-11 13:24:43 -0500

    chore(config/docs): restore settings

--- apps/docs/.source/index.js
+++
@@ -1,5 +0,0 @@
-import { toRuntime, toRuntimeAsync } from "fumadocs-mdx"
-import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=4f3bc6e48351eef47611191451d86bf2ebc0c4238119c3a0c8870a549ef72ca3"
-import * as docs_1 from "../content/docs/test.mdx?collection=docs&hash=4f3bc6e48351eef47611191451d86bf2ebc0c4238119c3a0c8870a549ef72ca3"
-export const docs = [toRuntime("doc", docs_0, {"path":"index.mdx","absolutePath":"/home/mfarabi/workspace/cuHacking/2025-backup/apps/docs/content/docs/index.mdx"}), toRuntime("doc", docs_1, {"path":"test.mdx","absolutePath":"/home/mfarabi/workspace/cuHacking/2025-backup/apps/docs/content/docs/test.mdx"})];
-export const meta = [];
--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -1,21 +1,37 @@
+// import type { Config } from 'tailwindcss'
+// const { fontFamily } = require('tailwindcss/defaultTheme')
 import { join } from 'node:path'
-import { createGlobPatternsForDependencies } from '@nx/react/tailwind'
+
 import { createPreset } from 'fumadocs-ui/tailwind-plugin'
 
+import { createGlobPatternsForDependencies } from '@nx/react/tailwind'
+
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    join(
-      __dirname,
-      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}',
-    ),
-    ...createGlobPatternsForDependencies(__dirname),
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './content/**/*.{md,mdx}',
-    './mdx-components.{ts,tsx}',
-    // './node_modules/fumadocs-ui/dist/**/*.js',
-    '../../node_modules/fumadocs-ui/dist/**/*.js',
-  ],
-  presets: [createPreset()],
+export const darkMode = ['class']
+export const content = [
+  join(
+    __dirname,
+    '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}',
+  ),
+  ...createGlobPatternsForDependencies(__dirname),
+  './mdx-components.tsx',
+  '../../node_modules/fumadocs-ui/dist/**/*.js',
+]
+export const theme = {
+  extend: {
+    fontFamily: {
+      // sans: ['var(--font-geist-sans)', ...fontFamily.sans],
+    },
+  },
 }
+export const presets = [
+  createPreset({
+    // preset: 'default',
+    // preset: 'neutral',
+    // preset: 'dusk',
+    // preset: 'purple',
+    // preset: 'ocean',
+    preset: 'catppuccin',
+  }),
+]
+export const plugins = []
