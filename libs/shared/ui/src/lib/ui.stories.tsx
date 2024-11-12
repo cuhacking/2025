3853f99f71ab575e3a8735e147721092c5cbaf49
Mumtahin Farabi <mfarabi619@gmail.com>
2024-11-11 21:51:39 -0500

    feat(libs/shared/ui): create project

---
+++ libs/shared/ui/src/lib/ui.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Ui from './ui'
+
+export default {
+  title: 'Example/Ui',
+  component: Ui,
+}
+
+export const Primary = () => <Ui />
---
+++ libs/shared/ui/src/lib/ui.tsx
@@ -0,0 +1,9 @@
+export function Ui() {
+  return (
+    <div>
+      <h1>Welcome to Ui!</h1>
+    </div>
+  )
+}
+
+export default Ui
