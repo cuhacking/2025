bd245b2d16a6b32e2c3c5550ca8ffbf94140359e
Mumtahin Farabi <mfarabi619@gmail.com>
2024-11-11 23:27:54 -0500

    chore(libs/shared/config): create copies of common setups

---
+++ libs/shared/config/eslint/base.js
@@ -0,0 +1,42 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  formatters: true,
+  // unocss: true,
+  react: true,
+  slidev: true,
+},
+// {}
+)
+
+// export default [
+//   ...nx.configs['flat/base'],
+//   ...nx.configs['flat/typescript'],
+//   ...nx.configs['flat/javascript'],
+//   {
+//     ignores: ['**/dist'],
+//   },
+//   {
+//     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+//     rules: {
+//       '@nx/enforce-module-boundaries': [
+//         'error',
+//         {
+//           enforceBuildableLibDependency: true,
+//           allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
+//           depConstraints: [
+//             {
+//               sourceTag: '*',
+//               onlyDependOnLibsWithTags: ['*'],
+//             },
+//           ],
+//         },
+//       ],
+//     },
+//   },
+//   {
+//     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+//     // Override or add rules here
+//     rules: {},
+//   },
+// ];
