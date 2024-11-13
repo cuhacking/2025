0ae80237d8fdaacf14277451ef6af3004d51d0e0
HasithDeAlwis <hasithde24@gmail.com>
2024-11-12 21:31:43 -0500

    feat(libs/shared/ui/cuhacking/components): create glass morphic card story

---
+++ libs/shared/ui/src/cuHacking/components/glassmorphic-card/glassmorphic-card.stories.tsx
@@ -0,0 +1,52 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import TerminalText from '../terminal-text/terminal-text'
+import GlassmorphicCard from './glassmorphic-card'
+
+const meta = {
+  title: 'cuHacking Design System/Glassmorphic Card',
+  component: GlassmorphicCard,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+    children: (
+      <>
+        <TerminalText callToAction>
+          Welcome to cuHacking
+        </TerminalText>
+        <TerminalText className="text-sm">
+          This is our terminal text!
+        </TerminalText>
+      </>
+    ),
+    variant: 'default',
+    className: 'px-1 py-2',
+  },
+  argTypes: {
+    variant: {
+      options: [
+        'default',
+        'info',
+        'nested',
+      ],
+      control: { type: 'select' },
+    },
+    children: {
+      control: {
+        disable: true,
+      },
+    },
+    className: {
+      control: { type: 'text' },
+    },
+  },
+} satisfies Meta<typeof GlassmorphicCard>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
+export const Nested: Story = { args: { variant: 'nested' } }
+export const Info: Story = { args: { variant: 'info' } }
+export const CustomStyles: Story = { args: { variant: 'default', className: 'px-3 py-4' } }
