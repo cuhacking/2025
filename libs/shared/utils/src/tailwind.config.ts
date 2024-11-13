3408db67a368fc24f5bfeb06852ab37252e171a5
HasithDeAlwis <hasithde24@gmail.com>
2024-11-12 21:22:00 -0500

    feat(libs/shared/utils/tailwind.config): extend default styles

--- libs/shared/utils/src/tailwind.config.ts
+++ libs/shared/utils/src/tailwind.config.ts
@@ -17,39 +17,59 @@ export function buildConfig(
     ],
     theme: {
       extend: {
+        fontFamily: {
+          sans: ['JetBrains Mono', 'sans-serif'],
+        },
+        backgroundImage: {
+          'greendiant': 'linear-gradient(to right, hsl(var(--greendiant-start)) 40%, hsl(var(--greendiant-end)) 60%)',
+          'g-keyboardBlack': 'linear-gradient(to right, hsl(var(--g-keyboardblack-start)) 30%, hsl(var(--background)) 60%)',
+          'g-nav-drawer-background': `
+            linear-gradient(
+              90deg,
+              hsl(var(--background)) 0%,
+              hsl(var(--black-light)) 25%,
+              hsl(var(--black-light)) 75%,
+              hsl(var(--background)) 100%
+            )
+          `,
+        },
         colors: {
-          border: 'hsl(var(--border))',
-          input: 'hsl(var(--input))',
-          ring: 'hsl(var(--ring))',
-          background: 'hsl(var(--background))',
-          foreground: 'hsl(var(--foreground))',
-          primary: {
+          'border': 'hsl(var(--border))',
+          'input': 'hsl(var(--input))',
+          'ring': 'hsl(var(--ring))',
+          'background': 'hsl(var(--background))',
+          'foreground': 'hsl(var(--foreground))',
+          'primary': {
             DEFAULT: 'hsl(var(--primary))',
             foreground: 'hsl(var(--primary-foreground))',
           },
-          secondary: {
+          'secondary': {
             DEFAULT: 'hsl(var(--secondary))',
             foreground: 'hsl(var(--secondary-foreground))',
           },
-          destructive: {
+          'destructive': {
             DEFAULT: 'hsl(var(--destructive))',
             foreground: 'hsl(var(--destructive-foreground))',
           },
-          muted: {
+          'muted': {
             DEFAULT: 'hsl(var(--muted))',
             foreground: 'hsl(var(--muted-foreground))',
           },
-          accent: {
+          'accent': {
             DEFAULT: 'hsl(var(--accent))',
             foreground: 'hsl(var(--accent-foreground))',
           },
-          popover: {
+          'popover': {
             DEFAULT: 'hsl(var(--popover))',
             foreground: 'hsl(var(--popover-foreground))',
           },
-          card: {
+          'card': {
             DEFAULT: 'hsl(var(--card))',
             foreground: 'hsl(var(--card-foreground))',
+          },
+          'card-nested': {
+            DEFAULT: 'hsl(var(--card-nested))',
+            foreground: 'hsl(var(--card-nested-foreground))',
           },
         },
         borderRadius: {
@@ -70,6 +90,10 @@ export function buildConfig(
         animation: {
           'accordion-down': 'accordion-down 0.2s ease-out',
           'accordion-up': 'accordion-up 0.2s ease-out',
+        },
+        boxShadow: {
+          dropShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+          innerShadow: 'inset 0 4px 4px rgba(0, 0, 0, 0.25)',
         },
       },
     },
