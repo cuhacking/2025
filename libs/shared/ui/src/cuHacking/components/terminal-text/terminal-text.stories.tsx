029c25e36afdb043e8bc03ff633b1dd4cf65867a
HasithDeAlwis <hasithde24@gmail.com>
2024-11-13 23:20:30 -0500

    feat(libs/shared/ui/cuhacking/components): create terminal text story

---
+++ libs/shared/ui/src/cuHacking/components/terminal-text/terminal-text.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { EIcons } from '@cuhacking/utils/enums/icons'
+import TerminalText from './terminal-text'
+
+const meta = {
+  title: 'cuHacking Design System/TerminalText',
+  component: TerminalText,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+    children: (
+      <>
+        <p>Hello, cuHacking enjoyers!!</p>
+      </>
+    ),
+    className: 'px-3 py-1',
+    callToAction: false,
+  },
+  argTypes: {
+    icon: {
+      options: Object.values(EIcons),
+      control: { type: 'select' },
+    },
+    children: {
+      control: {
+        disable: true,
+      },
+    },
+    callToAction: {
+      control: { type: 'check' },
+    },
+  },
+} satisfies Meta<typeof TerminalText>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
+export const ExtraLarge: Story = { args: { className: 'text-lg md:text-xl lg:text-3xl', callToAction: false } }
+export const CallToAction: Story = { args: { className: 'text-lg', callToAction: true } }
+export const Arrow: Story = { args: { className: 'text-lg', callToAction: false, icon: EIcons.ARROW } }
+export const Link: Story = { args: { className: 'text-lg', callToAction: false, icon: EIcons.LINK } }
+export const Calendar: Story = { args: { className: 'text-lg', callToAction: false, icon: EIcons.CALENDAR } }
+export const ChevronDown: Story = { args: { className: 'text-lg', callToAction: false, icon: EIcons.CHEVRON_DOWN } }
+export const Phone: Story = { args: { className: 'text-lg', callToAction: false, icon: EIcons.PHONE } }
+export const ChevronUp: Story = { args: { className: 'text-lg', callToAction: false, icon: EIcons.CHEVRON_UP } }
