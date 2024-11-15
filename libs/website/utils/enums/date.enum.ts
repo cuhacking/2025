4c908951aa7caab5ac311f99100f7713b41f7568
HasithDeAlwis <hasithde24@gmail.com>
2024-11-15 01:51:48 -0500

    feat(shared/website/utils/enum): create month shortform

---
+++ libs/website/utils/enums/date.enum.ts
@@ -0,0 +1,14 @@
+export enum EDate {
+  January = 'Jan',
+  February = 'Feb',
+  March = 'Mar',
+  April = 'Apr',
+  May = 'May',
+  June = 'Jun',
+  July = 'Jul',
+  August = 'Aug',
+  September = 'Sep',
+  October = 'Oct',
+  November = 'Nov',
+  December = 'Dec',
+}
