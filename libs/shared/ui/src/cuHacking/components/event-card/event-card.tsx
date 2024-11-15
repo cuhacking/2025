7a3f427c3b43e18bc328c858ad38f1da50b5f841
HasithDeAlwis <hasithde24@gmail.com>
2024-11-15 01:44:59 -0500

    feat(libs/shared/ui/src/cuhacking/components): create event card

---
+++ libs/shared/ui/src/cuHacking/components/event-card/event-card.tsx
@@ -0,0 +1,52 @@
+import type { IEvent } from '../../../../../../website/feature/events/models/event'
+import { Button } from '@cuhacking/ui/components/button/button'
+import GlassmorphicCard from '@cuhacking/ui/components/glassmorphic-card/glassmorphic-card'
+import { Separator } from '@cuhacking/ui/components/separator/separator'
+import TerminalText from '@cuhacking/ui/components/terminal-text/terminal-text'
+import { cn } from '@cuhacking/utils'
+import { cva } from 'class-variance-authority'
+import { getDatePostfix, getDayOfMonth, getMonth } from '../../../../../../website/utils/helpers/date.helpers'
+
+interface EventProps {
+  event: IEvent
+  className: string
+}
+function EventCard({ event, className }: EventProps) {
+  const date = event.date
+  const day = getDayOfMonth(date)
+  const month = getMonth(date)
+  const datePostfix = getDatePostfix(date)
+
+  const glassmorphicCardVariant = cva(
+    'flex flex-col lg:flex-row px-3 py-4 gap-x-3 gap-y-2.5 items-center',
+  )
+
+  return (
+    <GlassmorphicCard variant="nested" className={cn(glassmorphicCardVariant({ className }))}>
+      <section className="flex flex-col items-center">
+        <time className="text-3xl">
+          {day}
+          <sup>{datePostfix}</sup>
+        </time>
+        <time className="text-xs">
+          {month}
+        </time>
+      </section>
+      <Separator orientation="vertical" className="h-[100px] hidden lg:block" decorative />
+      <Separator orientation="horizontal" className="block w-full lg:hidden" decorative />
+      <article className="flex flex-col">
+        <header className="px-2">
+          <h4 className="text-2xl">{event.title}</h4>
+        </header>
+        <main>
+          <TerminalText className="text-sm">{event.description}</TerminalText>
+        </main>
+        <div className="flex justify-end w-full text-xl">
+          <Button variant="default" className="border" size="sm">REGISTER NOW</Button>
+        </div>
+      </article>
+    </GlassmorphicCard>
+  )
+}
+
+export default EventCard
