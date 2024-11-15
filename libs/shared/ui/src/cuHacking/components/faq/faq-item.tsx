3a314fe5ada606d25c8a31f8bde83e552f64fb74
HasithDeAlwis <hasithde24@gmail.com>
2024-11-15 11:29:04 -0500

    feat(shared/ui/scuhacking/components): create faq-items

---
+++ libs/shared/ui/src/cuHacking/components/faq/faq-item.tsx
@@ -0,0 +1,26 @@
+import { AccordionContent, AccordionItem, AccordionTrigger } from '@cuhacking/ui/components/accordion/accordion'
+import TerminalText from '@cuhacking/ui/components/terminal-text/terminal-text'
+
+interface FAQItemProps {
+  question: string
+  answer: string
+}
+
+function FAQItem({ question, answer }: FAQItemProps) {
+  return (
+    <AccordionItem value={question}>
+      <AccordionTrigger>
+        <p>
+          <span className="text-primary">faq(</span>
+          {question}
+          <span>)</span>
+        </p>
+      </AccordionTrigger>
+      <AccordionContent>
+        <TerminalText>{answer}</TerminalText>
+      </AccordionContent>
+    </AccordionItem>
+  )
+}
+
+export default FAQItem
