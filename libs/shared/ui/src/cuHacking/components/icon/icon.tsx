93520cf8919134cb7727ee82ff851aa306a024fa
HasithDeAlwis <hasithde24@gmail.com>
2024-11-19 15:45:04 -0500

    feat(shared/ui/cuhacking/components): create icon

---
+++ libs/shared/ui/src/cuHacking/components/icon/icon.tsx
@@ -0,0 +1,27 @@
+import type Media from '@cuhacking/types/media'
+import { cn } from '@shadcn/lib/utils'
+import React from 'react'
+
+interface IconProps {
+  media: Media
+  className?: string
+  prefix?: string
+}
+
+export function Icon({ media, className }: IconProps) {
+  return (
+    <>
+      { media
+        ? (
+            <img
+              src={media.src}
+              loading="lazy"
+              alt={media.alt}
+              className={cn('p-1', className)}
+            />
+          )
+        : null}
+    </>
+
+  )
+}
