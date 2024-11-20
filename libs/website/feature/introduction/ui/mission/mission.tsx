a190179288ca6f10e1a610d0c6fcf3863e02c936
HasithDeAlwis <hasithde24@gmail.com>
2024-11-20 09:14:54 -0500

    feat(libs/website/feature/introduction/mission): create mission section

---
+++ libs/website/feature/introduction/ui/mission/mission.tsx
@@ -0,0 +1,52 @@
+import { GlassmorphicCard } from '@cuhacking/shared/ui/src/cuHacking/components/glassmorphic-card'
+import { Separator } from '@cuhacking/shared/ui/src/cuHacking/components/separator'
+import { TerminalText } from '@cuhacking/shared/ui/src/cuHacking/components/terminal-text'
+import React from 'react'
+
+interface MissionProps {
+  logo: string
+}
+
+// TODO: Refactor so that it takes in props for text
+export function Mission({ logo }: MissionProps) {
+  return (
+
+    <GlassmorphicCard className="py-10 px-3.5 sm:px-12 sm:py-16">
+      <section className="flex flex-col items-center h-full gap-10 lg:flex-row">
+        <div>
+          <img src={logo} alt="cuHacking Logo" />
+        </div>
+        <Separator orientation="vertical" className="self-stretch hidden h-auto lg:block" />
+        <div className="flex flex-col gap-y-1">
+          <h2 className="text-3xl">OUR MISSION</h2>
+          <div>
+            <TerminalText className="text-base">
+              <p>
+                cuHacking is a 36-hour hackathon that
+                <span className="text-primary"> ✨sparks creativity</span>
+                {' '}
+                and problem-solving among 🍁Ottawa's brightest minds.
+              </p>
+
+            </TerminalText>
+            <TerminalText className="text-base">
+              <p>
+                {' '}
+                A catalyst for
+                <span className="text-orange-400"> innovation</span>
+                ,
+                <span className="text-blue-400"> collaboration</span>
+                , and
+                <span className="text-lime-400"> learning</span>
+                , providing an inclusive space for passionate minds to create and grow together.
+
+              </p>
+
+            </TerminalText>
+          </div>
+        </div>
+      </section>
+    </GlassmorphicCard>
+
+  )
+}
