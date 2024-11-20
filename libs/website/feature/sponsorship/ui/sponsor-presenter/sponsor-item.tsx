b8d8276d57f8bce77e01e0d700736822fd9210c0
HasithDeAlwis <hasithde24@gmail.com>
2024-11-19 21:16:13 -0500

    feat(libs/website/feature/sponsorship): create section

---
+++ libs/website/feature/sponsorship/ui/sponsor-presenter/sponsor-item.tsx
@@ -0,0 +1,36 @@
+import type { Sponsor } from '../../types/sponsorship'
+import { GlassmorphicCard } from '@cuhacking/shared/ui/src/cuHacking/components/glassmorphic-card'
+import { cn } from '@shadcn/lib/utils'
+import { cva } from 'class-variance-authority'
+import React from 'react'
+
+interface SponsorPresenterProps {
+  sponsor: Sponsor
+  isPresent: boolean
+}
+
+const sponsorPresenterVariation = cva(
+  'p-2.5 hover:scale-105 transition-transform',
+  {
+    variants: {
+      isPresent: {
+        true: 'h-32 min-w-24',
+        false: 'h-24 min-w-16',
+      },
+    },
+  },
+)
+export function SponsorItem({ sponsor, isPresent }: SponsorPresenterProps) {
+  return (
+    <GlassmorphicCard variant="nested" className="p-2.5 hover:scale-105 transition-transform">
+      <a
+        href={sponsor.link}
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label={`Visit ${sponsor.name}'s website`}
+      >
+        <img src={sponsor.logo} alt={`${sponsor.name} logo`} loading="lazy" className={cn(sponsorPresenterVariation({ isPresent }))} />
+      </a>
+    </GlassmorphicCard>
+  )
+}
---
+++ libs/website/feature/sponsorship/ui/sponsorship-section.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { SPONSORSHIP_CONSTANTS } from '../constants/sponsorship.constants'
+import { SponsorshipPresenter } from './sponsorship-presenter/sponsorship.presenter'
+
+export function SponsorshipSection() {
+  return (
+    <div className="flex justify-center w-full">
+      <div className="w-full max-w-screen-xl px-5 py-5 lg:px-20 lg:py-14">
+        <section id="sponsors" className="w-full">
+          <SponsorshipPresenter
+            sponsors={SPONSORSHIP_CONSTANTS.SPONSORS}
+            text={SPONSORSHIP_CONSTANTS.TEXT}
+          />
+        </section>
+      </div>
+    </div>
+  )
+}
