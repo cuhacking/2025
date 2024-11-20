2d1b81685cfee85194461fb92d01df8ce36fc714
HasithDeAlwis <hasithde24@gmail.com>
2024-11-19 22:25:12 -0500

    feat(libs/website/feature/introduction/welcome): create welcome section

---
+++ libs/website/feature/introduction/ui/welcome/welcome.section.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { ClientOnly } from 'remix-utils/client-only'
+import { WELCOME_CONSTATNTS } from '../../constants/welcome.constants'
+import { SplineWelcome } from './spline-welcome.client'
+import { Welcome } from './welcome'
+
+export function WelcomeSection() {
+  return (
+    <section className="relative flex justify-center w-full mx-auto lg:mx-0">
+      <ClientOnly>
+        {() => (
+          <SplineWelcome />
+        )}
+      </ClientOnly>
+      <div className="w-full h-screen max-w-screen-xl px-5 py-5 lg:px-20 lg:py-14">
+        <section className="w-full lg:w-3/5">
+          <Welcome socials={WELCOME_CONSTATNTS.SOCIALS} />
+        </section>
+      </div>
+    </section>
+  )
+}
