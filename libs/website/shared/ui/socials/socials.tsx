5ec02915715510988fb5422b36e1154713de2120
HasithDeAlwis <hasithde24@gmail.com>
2024-11-19 22:15:10 -0500

    feat(libs/website/shared/ui): create socials component

---
+++ libs/website/shared/ui/socials/socials.tsx
@@ -0,0 +1,24 @@
+import type { Media } from '@cuhacking/shared/types/media'
+import { Icon } from '@cuhacking/shared/ui/src/cuHacking/components/icon'
+import { cn } from '@shadcn/lib/utils'
+import React from 'react'
+
+interface SocialsProps {
+  socials: {
+    link: string
+    media: Media
+  }[]
+  className: string
+}
+
+export function Socials({ socials, className }: SocialsProps) {
+  return (
+    <div className={cn('flex flex-wrap gap-x-5 gap-y-3', className)}>
+      {socials.map(({ link, media }) => (
+        <a key={link} href={link}>
+          <Icon media={media} prefix="/socials" />
+        </a>
+      ))}
+    </div>
+  )
+}
