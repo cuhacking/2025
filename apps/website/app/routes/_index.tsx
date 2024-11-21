7404ef8d22091918915ee63aba8b672dc9bcb373
HasithDeAlwis <hasithde24@gmail.com>
2024-11-20 21:57:09 -0500

    feat(apps/website): serve website home page

--- apps/website/app/routes/_index.tsx
+++ apps/website/app/routes/_index.tsx
@@ -1,17 +1,8 @@
-import { Icon } from '../../../../libs/shared/ui/src/cuHacking/components/icon/icon'
+import { Home } from '@website/pages/home'
 // import { Icon } from '@cuhacking/shared/ui/src/cuHacking/components/icon/icon'
-import cuHackingLogo from '../../public/cuhacking-logo.svg'
 
 export default function Index() {
-  const media = {
-    src: cuHackingLogo,
-    alt: 'logo',
-  }
   return (
-    <div className="container flex justify-center mx-auto">
-      <div className="max-w-lg">
-        <Icon media={media} />
-      </div>
-    </div>
+    <Home />
   )
 }
