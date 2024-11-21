bcfc725cc33057f04581d489557c247f5c882dcb
HasithDeAlwis <hasithde24@gmail.com>
2024-11-20 21:51:18 -0500

    feat(libs/website/layout): create basic layout

---
+++ libs/website/layouts/base.tsx
@@ -0,0 +1,23 @@
+import { FOOTER_CONSTANTS, FooterPresenter, NAVBAR_CONSTANTS, NavbarPresenter } from '@website/shared/ui/navigation'
+import React from 'react'
+import { ClientOnly } from 'remix-utils/client-only'
+
+export function Layout({ children }) {
+  return (
+    <>
+      <ClientOnly>
+        {() => (
+          <NavbarPresenter
+            links={NAVBAR_CONSTANTS.LINKS}
+            logo={NAVBAR_CONSTANTS.LOGO}
+            socials={NAVBAR_CONSTANTS.SOCIALS}
+            hamburger={NAVBAR_CONSTANTS.HAMBURGER}
+          />
+        )}
+      </ClientOnly>
+
+      <div className="mt-10">{children}</div>
+      <FooterPresenter logo={FOOTER_CONSTANTS.LOGO} socials={FOOTER_CONSTANTS.SOCIALS} />
+    </>
+  )
+}
