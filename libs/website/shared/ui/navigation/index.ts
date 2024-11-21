087cc6970d01f3975b54c43481c77e6b2ab73041
HasithDeAlwis <hasithde24@gmail.com>
2024-11-20 21:55:46 -0500

    feat(libs/website/shared/navigation): create navigation entry point

---
+++ libs/website/shared/ui/navigation/index.ts
@@ -0,0 +1,4 @@
+export { FOOTER_CONSTANTS } from './footer/constants/footer.constants'
+export { FooterPresenter } from './footer/ui/footer.presenter'
+export { NAVBAR_CONSTANTS } from './navbar/constants/navbar.constants'
+export { NavbarPresenter } from './navbar/ui/navbar.presenter'
