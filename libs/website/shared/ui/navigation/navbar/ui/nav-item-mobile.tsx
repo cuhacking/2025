1d31d51d76c481bcb3828d779c6c47a68b9f1eef
HasithDeAlwis <hasithde24@gmail.com>
2024-11-24 11:00:46 -0500

    refactor: use Remix Link instead of default <a> tag

--- libs/website/shared/ui/navigation/navbar/ui/nav-item-mobile.tsx
+++ libs/website/shared/ui/navigation/navbar/ui/nav-item-mobile.tsx
@@ -1,13 +1,12 @@
+import { Link } from '@remix-run/react'
 import React from 'react'
 
 export function MobileNavItem({ link, name }) {
   return (
     <div className="p-2.5">
-      <a href={link} className="block" aria-label={`Navigate to ${name}`}>
-        <h2 className="text-[48px] font-extrabold">
-          {name}
-        </h2>
-      </a>
+      <Link to={link} className="block" aria-label={`Navigate to ${name}`}>
+        <h2 className="text-[48px] font-extrabold">{name}</h2>
+      </Link>
     </div>
   )
 }
