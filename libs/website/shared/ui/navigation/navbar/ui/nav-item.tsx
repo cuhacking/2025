f0966585d9c88b335216758f5ec94ad9698a58c6
HasithDeAlwis <hasithde24@gmail.com>
2024-11-24 11:00:46 -0500

    refactor(web): use Remix Link instead of default <a> tag

--- libs/website/shared/ui/navigation/navbar/ui/nav-item.tsx
+++ libs/website/shared/ui/navigation/navbar/ui/nav-item.tsx
@@ -1,15 +1,14 @@
-import { NavigationMenuLink } from '@cuhacking/shared/ui/src/cuHacking/components/navigation-menu'
-// import { Link } from '@remix-run/react'
+import { Link } from '@remix-run/react'
 import React from 'react'
 // TODO: Refactor to use <Link> from Remix
 export function NavItem({ index, link, name }) {
   return (
-    <NavigationMenuLink href={link}>
+    <Link to={link}>
       <span className="text-xs text-border">
         {String(index + 1).padStart(2, '0')}
         /
       </span>
       {name}
-    </NavigationMenuLink>
+    </Link>
   )
 }
