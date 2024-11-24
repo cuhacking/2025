3c5f0bc66948eae3555030560d15398ac31548db
HasithDeAlwis <hasithde24@gmail.com>
2024-11-24 10:08:54 -0500

    refactor: remove dependency from shadcn

--- libs/website/feature/events/ui/event-item/event-item.tsx
+++ libs/website/feature/events/ui/event-item/event-item.tsx
@@ -3,9 +3,13 @@ import { Button } from '@cuhacking/shared/ui/src/cuHacking/components/button'
 import { GlassmorphicCard } from '@cuhacking/shared/ui/src/cuHacking/components/glassmorphic-card'
 import { Separator } from '@cuhacking/shared/ui/src/cuHacking/components/separator'
 import { TerminalText } from '@cuhacking/shared/ui/src/cuHacking/components/terminal-text'
-import { getDatePostfix, getDayOfMonth, getMonth } from '@cuhacking/shared/utils/date.helpers'
+import { cn } from '@cuhacking/shared/utils/cn'
+import {
+  getDatePostfix,
+  getDayOfMonth,
+  getMonth,
+} from '@cuhacking/shared/utils/date.helpers'
 import { Link } from '@remix-run/react'
-import { cn } from '@shadcn/lib/utils'
 import React from 'react'
 
 interface EventProps {
@@ -17,20 +21,33 @@ export function EventItem({ event, className }: EventProps) {
   const day = getDayOfMonth(date)
   const month = getMonth(date)
   const datePostfix = getDatePostfix(date)
-  const buttonMessage = event.status === 'upcoming' ? 'REGISTER NOW' : 'VIEW PHOTOS'
+  const buttonMessage
+    = event.status === 'upcoming' ? 'REGISTER NOW' : 'VIEW PHOTOS'
   return (
-    <GlassmorphicCard variant="nested" className={cn('flex flex-col lg:flex-row px-3 py-4 gap-x-3 gap-y-2.5 items-center h-full', className)}>
+    <GlassmorphicCard
+      variant="nested"
+      className={cn(
+        'flex flex-col lg:flex-row px-3 py-4 gap-x-3 gap-y-2.5 items-center h-full',
+        className,
+      )}
+    >
       <section className="flex flex-col items-center">
         <time className="text-3xl">
           {day}
           <sup>{datePostfix}</sup>
         </time>
-        <time className="text-xs font-bold">
-          {month}
-        </time>
+        <time className="text-xs font-bold">{month}</time>
       </section>
-      <Separator orientation="vertical" className="self-stretch hidden h-auto lg:block" decorative />
-      <Separator orientation="horizontal" className="block w-full lg:hidden" decorative />
+      <Separator
+        orientation="vertical"
+        className="self-stretch hidden h-auto lg:block"
+        decorative
+      />
+      <Separator
+        orientation="horizontal"
+        className="block w-full lg:hidden"
+        decorative
+      />
       <article className="flex flex-col">
         <header className="px-2">
           <h4 className="text-2xl">{event.title}</h4>
@@ -41,7 +58,11 @@ export function EventItem({ event, className }: EventProps) {
         {event.status !== 'in-progress'
           ? (
               <div className="flex justify-end w-full pt-1 text-xl">
-                <Button variant="default" size="sm"><Link to={event.link} target="_blank">{buttonMessage}</Link></Button>
+                <Button variant="default" size="sm">
+                  <Link to={event.link} target="_blank">
+                    {buttonMessage}
+                  </Link>
+                </Button>
               </div>
             )
           : null}
