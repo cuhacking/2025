e7b0fc1222f6d166ab4f35331366eebbaf1d9a03
Mumtahin Farabi <mfarabi619@gmail.com>
2024-11-26 17:57:09 -0500

    fix(config/shared/eslint): missing nx import

--- libs/shared/config/eslint/default.js
+++ libs/shared/config/eslint/default.js
@@ -1,3 +1,5 @@
+import nx from '@nx/eslint-plugin'
+
 export default [
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
@@ -28,4 +30,4 @@ export default [
     // Override or add rules here
     rules: {},
   },
-];
+]
