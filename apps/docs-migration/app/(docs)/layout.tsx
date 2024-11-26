4683f999b0547df0715c6ac408f1bf21f5f1936f
Mumtahin Farabi <mfarabi619@gmail.com>
2024-11-26 00:50:47 -0500

    feat(ui/docs/layout): remove root toggle

--- apps/docs-migration/app/(docs)/layout.tsx
+++ apps/docs-migration/app/(docs)/layout.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from 'react'
-import { RootToggle } from 'fumadocs-ui/components/layout/root-toggle'
-import { pages, source } from '../../source'
+/* import { RootToggle } from 'fumadocs-ui/components/layout/root-toggle' */
+import { source } from '../../source'
 /* https://fumadocs.vercel.app/docs/ui/blocks/layout#notebook */
 /* import { DocsLayout } from 'fumadocs-ui/layouts/docs' */
 import { DocsLayout } from 'fumadocs-ui/layouts/notebook'
@@ -11,26 +11,26 @@ export default function Layout({ children }: { children: ReactNode }) {
     <DocsLayout
       tree={source.pageTree}
       {...baseOptions}
-      sidebar={{
-        banner: (
-          <RootToggle
-            options={pages.map(page => ({
-              title: page.title,
-              description: page.description,
-              url: `/${page.url}`,
-              icon: (
-                <page.icon
-                  className="size-9 shrink-0 rounded-md bg-gradient-to-t from-background/80 p-1.5"
-                  style={{
-                    backgroundColor: `hsl(var(--primary)/.3)`,
-                    color: `hsl(var(--accent-foreground))`,
-                  }}
-                />
-              ),
-            }))}
-          />
-        ),
-      }}
+    /* sidebar={{
+     *   banner: (
+     *     <RootToggle
+     *       options={pages.map(page => ({
+     *         title: page.title,
+     *         description: page.description,
+     *         url: `/${page.url}`,
+     *         icon: (
+     *           <page.icon
+     *             className="size-9 shrink-0 rounded-md bg-gradient-to-t from-background/80 p-1.5"
+     *             style={{
+     *               backgroundColor: `hsl(var(--primary)/.3)`,
+     *               color: `hsl(var(--accent-foreground))`,
+     *             }}
+     *           />
+     *         ),
+     *       }))}
+     *     />
+     *   ),
+     * }} */
     >
       {children}
     </DocsLayout>
