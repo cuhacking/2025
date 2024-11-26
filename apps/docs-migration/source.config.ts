1247a4978224bf488794abd8b0068425dfaaaf13
Mumtahin Farabi <mfarabi619@gmail.com>
2024-11-26 12:51:23 -0500

    fix(config/docs/source): math equations not rendering

--- apps/docs-migration/source.config.ts
+++ apps/docs-migration/source.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig, defineDocs } from 'fumadocs-mdx/config'
+import rehypeKatex from 'rehype-katex'
+import remarkMath from 'remark-math'
 
 export const { docs, meta } = defineDocs({
   dir: 'content/docs',
 })
 
-export default defineConfig()
+export default defineConfig({
+  mdxOptions: {
+    remarkPlugins: [remarkMath],
+    // Place it at first so that it won't be changed by syntax highlighter
+    rehypePlugins: v => [rehypeKatex, ...v],
+  },
+},
+)
