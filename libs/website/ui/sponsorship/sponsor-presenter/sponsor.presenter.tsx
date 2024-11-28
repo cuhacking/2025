87f321bb36465362afab6223576a5dcc06de1d0a
HasithDeAlwis <hasithde24@gmail.com>
2024-11-28 01:35:50 -0500

    feat(ui/web/sponsorship): update the sponsorship section to look nice lol

--- libs/website/ui/sponsorship/sponsor-presenter/sponsor.presenter.tsx
+++ libs/website/ui/sponsorship/sponsor-presenter/sponsor.presenter.tsx
@@ -13,7 +13,7 @@ export function SponsorPresenter({ isPresent, sponsors }: SponsorDisplayProps) {
       <TerminalText>
         {isPresent ? <h2> Present </h2> : <h2> Past </h2>}
       </TerminalText>
-      <main className="flex flex-row flex-wrap gap-y-3 justify-center lg:items-start">
+      <main className="flex flex-row flex-wrap gap-3 justify-center md:justify-start lg:items-start">
         {sponsors.map(sponsor => (
           <div key={sponsor.name} className="lg:pl-9">
             <SponsorItem sponsor={sponsor} isPresent={isPresent} />
