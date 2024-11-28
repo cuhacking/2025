7740c8387b06e41ee3b805fd011f2e030463b519
HasithDeAlwis <hasithde24@gmail.com>
2024-11-28 01:38:01 -0500

    refactor(ui/web/spline): use error boundaries

--- libs/website/ui/introduction/welcome/welcome.section.tsx
+++ libs/website/ui/introduction/welcome/welcome.section.tsx
@@ -1,13 +1,20 @@
+import { SplineComponent } from '@website/shared/ui/spline/spline.client'
 import React from 'react'
 import { ClientOnly } from 'remix-utils/client-only'
 import { WELCOME_CONSTATNTS } from '../constants/welcome.constants'
-import { SplineWelcome } from './spline-welcome.client'
 import { Welcome } from './welcome'
 
 export function WelcomeSection() {
   return (
     <section className="relative flex justify-center w-full mx-auto lg:mx-0">
-      <SplineWelcome />
+      <ClientOnly>
+        {() => (
+          <SplineComponent
+            className="absolute -bottom-16 lg:left-0 lg:top-0 lg:scale-[1.2]"
+            link="https://prod.spline.design/nnjZJFW1wThAacUS/scene.splinecode"
+          />
+        )}
+      </ClientOnly>
       <div className="w-full h-screen max-w-screen-xl px-5 py-5 lg:px-20 lg:py-14">
         <section className="w-full lg:w-3/5">
           <Welcome socials={WELCOME_CONSTATNTS.SOCIALS} />
