041c7772ebff250c87f70e8737160c5e488ee870
HasithDeAlwis <hasithde24@gmail.com>
2024-11-28 02:17:02 -0500

    feat(ui/web/navbar): add mobile animations

--- libs/website/shared/ui/navigation/navbar/ui/nav-item-mobile.tsx
+++ libs/website/shared/ui/navigation/navbar/ui/nav-item-mobile.tsx
@@ -1,9 +1,14 @@
 import { Link } from '@remix-run/react'
 import React from 'react'
 
-export function MobileNavItem({ link, name }) {
+interface MobileNavItemProps {
+  link: string
+  name: string
+  onClick: () => void
+}
+export function MobileNavItem({ link, name, onClick }: MobileNavItemProps) {
   return (
-    <div className="p-2.5">
+    <div onClick={onClick} className="p-2.5">
       <Link to={link} className="block" aria-label={`Navigate to ${name}`}>
         <h2 className="text-[48px] font-extrabold">{name}</h2>
       </Link>
