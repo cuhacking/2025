f89af4dac4bd57d30b415de69548d95371a7fe4c
JeremyFriesenGitHub <jeremyfriesen12@gmail.com>
2024-11-28 18:46:09 -0500

    chore: commit

--- tools/risk-form-filler/helpers/test.ts
+++
@@ -1,9 +0,0 @@
-import { promptUser } from './cli'
-import { createMockStreams } from './mock-cli'
-
-async function testCLI() {
-  const { stdin, stdout } = createMockStreams(['yes'])
-  await promptUser('What is your name? ', stdin, stdout)
-}
-
-testCLI().catch(err => console.error(err))
