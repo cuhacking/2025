221a14e13e52cfa416a22da3c8f37634fc4df28a
HasithDeAlwis <hasithde24@gmail.com>
2024-12-02 17:23:42 -0500

    perf(ui/web)!: slow load times from spline components

--- libs/website/shared/ui/spline/spline-component.tsx
+++ libs/website/shared/ui/spline/spline-component.tsx
@@ -1,17 +1,23 @@
-import { cn } from '@cuhacking/shared/utils/cn'
-import { ErrorBoundary } from '@cuhacking/shared/utils/ErrorBoundary'
-import { lazy, Suspense } from 'react'
+import { cn } from '@cuhacking/shared/utils/cn';
+/* import { ErrorBoundary } from '@cuhacking/shared/utils/ErrorBoundary' */
+import { lazy, Suspense } from 'react';
 
-const Spline = lazy(() => import('@splinetool/react-spline'))
+const Spline = lazy(() => import('@splinetool/react-spline'));
 
-export function SplineComponent({ link, className }: { link: string, className: string }) {
+export function SplineComponent({
+  link,
+  className,
+}: {
+  link: string;
+  className: string;
+}) {
   return (
     <div className="overflow-x-hidden">
-      <ErrorBoundary>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Spline className={cn(className)} scene={link} />
-        </Suspense>
-      </ErrorBoundary>
+      {/* <ErrorBoundary> */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <Spline className={cn(className)} scene={link} />
+      </Suspense>
+      {/* </ErrorBoundary> */}
     </div>
-  )
+  );
 }
--- libs/website/ui/introduction/mission/mission.section.tsx
+++ libs/website/ui/introduction/mission/mission.section.tsx
@@ -1,22 +1,22 @@
-import { SplineComponent } from '@website/shared/ui/spline/spline-component'
-import { MISSION_CONSTANTS } from '../constants/mission.constants'
-import { Mission } from './mission'
+/* import { SplineComponent } from '@website/shared/ui/spline/spline-component'; */
+import { MISSION_CONSTANTS } from '../constants/mission.constants';
+import { Mission } from './mission';
 
 export function MissionSection() {
-  return (
-    <section id="#about" className="relative flex justify-center w-full">
-      <SplineComponent
+    return (
+        <section id="#about" className="relative flex justify-center w-full">
+            {/* <SplineComponent
         className="absolute -bottom-[15vh] lg:-bottom-[5vh] left-0 scale-1 md:scale-[1.2]"
         link="https://prod.spline.design/TGlqj05806lq8PRV/scene.splinecode"
-      />
-      <main className="max-w-screen-xl px-5 py-5 lg:px-20 lg:py-14">
-        <div
-          id="about"
-          className="relative min-h-[160vh] lg:min-h-screen w-full"
-        >
-          <Mission logo={MISSION_CONSTANTS.LOGO} />
-        </div>
-      </main>
-    </section>
-  )
+      /> */}
+            <main className="max-w-screen-xl px-5 py-5 lg:px-20 lg:py-14">
+                <div
+                    id="about"
+                    className="relative min-h-[160vh] lg:min-h-screen w-full"
+                >
+                    <Mission logo={MISSION_CONSTANTS.LOGO} />
+                </div>
+            </main>
+        </section>
+    );
 }
