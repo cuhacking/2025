b2d72fd644fed3621490c9813a8980a2297de859
saim hashmi <hashmisaim037@gmail.com>
2024-12-08 04:57:50 -0500

    feat(contact): updated code for E2E tests

--- libs/website/feature/contactpage/ui/ContactHero.tsx
+++ libs/website/feature/contactpage/ui/ContactHero.tsx
@@ -4,9 +4,9 @@ import React from 'react'
 export function ContactHero(): React.JSX.Element {
   return (
     <div className="mb-8">
-      <h1 className="text-3xl font-bold mb-3 pl-3 sm:pl-8">
+      <h2 className="text-3xl font-bold mb-3 pl-3 sm:pl-8">
         Contact Us
-      </h1>
+      </h2>
       <p className="text-lg pl-5 sm:pl-10">
         <TerminalText>
           Feel free to ask us anything! We’re here to help.
