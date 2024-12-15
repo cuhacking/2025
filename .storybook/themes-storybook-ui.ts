dfde0d877c4dc524a776f1985b7575d3c479bc1b
Mumtahin Farabi <mfarabi619@gmail.com>
2024-12-14 21:51:00 -0500

    feat(ui/storybook): theme with design system tokens

--- .storybook/themes-storybook-ui.ts
+++ .storybook/themes-storybook-ui.ts
@@ -2,111 +2,79 @@
 
 export const darkUIStorybook = {
   base: 'dark',
-  colorPrimary: '#FF4785',
-  colorSecondary: '#029CFD',
-
-  // --primary: 210 40% 98%;
-  // --primary-foreground: 222.2 47.4% 11.2%;
-  // colorPrimary: 'hsl(210 40% 98%)',
-
-  // --secondary: 217.2 32.6% 17.5%;
-  // --secondary-foreground: 210 40% 98%;
-  // colorSecondary: 'hsl(217.2 32.6% 17.5%)',
+  colorPrimary: 'hsl(333.3 71.4% 50.6%)',
+  colorSecondary: 'hsl(87 87% 35%)',
 
   // UI
-  // appBg: '#222425',
-  // appContentBg: '#1B1C1D',
-  // appPreviewBg: '#FFFFFF',
-  // appBorderColor: 'rgba(255,255,255,.1)',
-  // appBorderRadius: 4,
+  // Sidebar
+  appBg: 'hsl(0 0% 0%)',
 
-  // --background: 222.2 84% 4.9%;
-  // --foreground: 210 40% 98%;
-  appBg: 'hsl(222.2 84% 4.9%)',
-  appContentBg: 'hsl(222.2 84% 4.9%)',
-  appPreviewBg: 'hsl(210 40% 98%)',
-  // --border: 217.2 32.6% 17.5%;
-  // --input: 217.2 32.6% 17.5%;
-  // --ring: 212.7 26.8% 83.9%;
-  appBorderColor: 'hsl(217.2 32.6% 17.5%)',
+  appContentBg: 'hsl(0 0% 0%)',
+  appPreviewBg: 'hsl(0 0% 0%)',
+  appBorderColor: 'hsla(0 0% 100% / 15%)',
+
+  // TYPOGRAPHY
+  textColor: 'hsl(0 0% 100%)',
+  textInverseColor: 'hsl(116 0% 0%)',
+  // textMutedColor: 'hsl(78 10% 15%)',
+
+  // TOOLBAR AND ACTION BAR
+  barTextColor: 'hsl(0 0% 100%)', // toolbar and action bar text color
+  barHoverColor: 'hsl(47.9 95.8% 53.1%)',
+  barSelectedColor: 'hsl(47.9 95.8% 53.1%)',
+  barBg: 'hsl(20.5 0% 0%)',
+
+  buttonBg: 'hsl(87 87% 5%)', // plus button for creating new story
+  buttonBorder: 'hsla(87 87% 35% / 35%)',
+  booleanBg: 'hsl(47.9 95.8% 8%)',
+  booleanSelectedBg: 'hsl(47.9 95.8% 20%)',
+
+  // FORMS
+  inputBg: 'hsl(47.9 95.8% 5%)',
+  inputBorder: 'hsla(20.5 90.2% / 15%)',
+  inputTextColor: 'hsl(0 0% 100%)',
+
+  // RADIUS
   appBorderRadius: 8,
-
-  // Typography
-  fontBase: '"Nunito Sans", -apple-system, ".SFNSText-Regular", "San Francisco", BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", Helvetica, Arial, sans-serif',
-  fontCode: 'ui-monospace, Menlo, Monaco, "Roboto Mono", "Oxygen Mono", "Ubuntu Monospace", "Source Code Pro", "Droid Sans Mono", "Courier New", monospace',
-  textColor: '#C9CDCF',
-  textInverseColor: '#222425',
-  textMutedColor: '#798186',
-
-  // fontBase: "",
-  // fontCode: '',
-  // textColor: 'hsl(210 40% 98%)',
-  // textInverseColor: 'hsl(217.2 32.6% 17.5%)',
-  // textMutedColor: 'hsl(217.2 32.6% 17.5%)',
-
-  // Toolbar and action bar
-  // barTextColor: '#73828C',
-  barHoverColor: '#029CFD',
-  barSelectedColor: '#029CFD',
-  // barBg: '#292C2E',
-
-  barTextColor: 'hsl(210 40% 98%)', // toolbar and action bar text color
-  // barHoverColor: string;
-  // barSelectedColor: 'hsl(210 40% 98%)',
-  barBg: 'hsl(222.2 84% 4.9%)',
-
-  // buttonBg: '#222425', // plus button for creating new story
-  buttonBorder: 'rgba(255,255,255,.1)',
-  booleanBg: '#222425',
-  booleanSelectedBg: '#2E3438',
-
-  buttonBg: 'hsl(222.2 84% 4.9%)',
-  // buttonBorder: string;
-  // booleanBg: string;
-  // booleanSelectedBg: string;
-
-  // Forms
-  inputBg: '#1B1C1D',
-  inputBorder: 'rgba(255,255,255,.1)',
-  inputTextColor: '#FFFFFF',
-  inputBorderRadius: 4,
-
-  // inputBg: 'hsl(217.2 32.6% 17.5%)',
-  // inputBorder: 'hsl(217.2 32.6% 17.5%)',
-  // inputTextColor: 'hsl(210 40% 98%)',
-  // inputBorderRadius: '0.4rem',
+  inputBorderRadius: 8,
 }
 
 export const lightUIStorybook = {
-  //  --background: 0 0% 100%;
-  //  --foreground: 222.2 84% 4.9%;
-  //
-  //  --card: 0 0% 100%;
-  //  --card-foreground: 222.2 84% 4.9%;
-  //
-  //  --popover: 0 0% 100%;
-  //  --popover-foreground: 222.2 84% 4.9%;
-  //
-  //  --primary: 222.2 47.4% 11.2%;
-  //  --primary-foreground: 210 40% 98%;
-  //
-  //  --secondary: 210 40% 96.1%;
-  //  --secondary-foreground: 222.2 47.4% 11.2%;
-  //
-  //  --muted: 210 40% 96.1%;
-  //  --muted-foreground: 215.4 16.3% 46.9%;
-  //
-  //  --accent: 210 40% 96.1%;
-  //  --accent-foreground: 222.2 47.4% 11.2%;
-  //
-  //  --destructive: 0 84.2% 60.2%;
-  //  --destructive-foreground: 210 40% 98%;
-  //
-  //  --border: 214.3 31.8% 91.4%;
-  //  --input: 214.3 31.8% 91.4%;
-  //  --ring: 222.2 84% 4.9%;
-  //
-  //  --radius: 0.5rem;
+  colorPrimary: 'hsl(333.3 71.4% 50.6%)',
+  colorSecondary: 'hsl(87 87% 35%)',
+
+  // UI
+  // Sidebar
+  appBg: 'hsl(116 0% 95%)',
+
+  appContentBg: 'hsl(116 0% 95%)',
+  appPreviewBg: 'hsl(116 0% 95%)',
+  appBorderColor: 'hsl(116 20% 50%)',
+
+  // TYPOGRAPHY
+  textColor: 'hsl(116 0% 0%)',
+  textInverseColor: 'hsl(116 0% 95%)',
+  // textMutedColor: 'hsl(78 10% 15%)',
+
+  // TOOLBAR AND ACTION BAR
+  // barTextColor: 'hsl(116 0% 0%)', // toolbar and action bar text color
+  barHoverColor: 'hsl(20.5 90.2% 48.2%)',
+  barSelectedColor: 'hsl(20.5 90.2% 48.2%)',
+  barBg: 'hsl(116 0% 100%)',
+
+  buttonBg: 'hsl(87 87% 95%)', // plus button for creating new story
+  buttonBorder: 'hsla(87 87% 35% / 35%)',
+  booleanBg: 'hsl(20.5 90.2% 90%)',
+  booleanSelectedBg: 'hsl(20.5 90.2% 80%)',
+
+  // FORMS
+  inputBg: 'hsl(20.5 90.2% 90%)',
+  inputBorder: 'hsl(20.5 20% 50%)',
+  inputTextColor: 'hsl(116 0% 0%)',
+
+  // RADIUS
+  appBorderRadius: 8,
+  inputBorderRadius: 8,
 }
 
 export const commonTheme = {
@@ -114,4 +82,8 @@ export const commonTheme = {
   brandUrl: '/',
   brandTarget: '_self',
   brandImage: 'https://cuhacking.ca/Logo-2025.svg',
+
+  // TYPOGRAPHY
+  fontBase: 'Jetbrains Mono',
+  fontCode: 'Jetbrains Mono',
 }
