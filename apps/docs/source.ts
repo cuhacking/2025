f7a29f025062b548352e2a61d4252e0c9cad5ac0
Mumtahin Farabi <mfarabi619@gmail.com>
2024-12-16 22:20:35 -0500

    docs(overview): update content

--- apps/docs/source.ts
+++ apps/docs/source.ts
@@ -1,41 +1,9 @@
 import { loader } from 'fumadocs-core/source'
 import { createMDXSource } from 'fumadocs-mdx'
-import {
-  GitPullRequestCreateArrow as ContributionGuidelinesIcon,
-  Dock as HomeIcon,
-  icons,
-  BookOpenText as KnowledgeBaseIcon,
-  Layers as ToolsIcon,
-} from 'lucide-react'
+import { icons } from 'lucide-react'
 import { createElement } from 'react'
 import { docs, meta } from './.source'
 
-export const pages = [
-  {
-    title: 'Home',
-    description: 'Leave none behind regardless of our development cost.',
-    url: '',
-    icon: HomeIcon,
-  },
-  {
-    title: 'Tech Stack',
-    description: 'For those wondering about our specs.',
-    url: 'tech-stack',
-    icon: ToolsIcon,
-  },
-  {
-    title: 'Contribution Guidelines',
-    description: 'Understand our collaboration process.',
-    url: 'contribution-guidelines',
-    icon: ContributionGuidelinesIcon,
-  },
-  {
-    title: 'Knowledge Base',
-    description: 'Curated resources to accelerate your learning.',
-    url: 'knowledge-base',
-    icon: KnowledgeBaseIcon,
-  },
-]
 export const source = loader({
   baseUrl: '/',
 
