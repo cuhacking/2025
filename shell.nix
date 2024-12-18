2d10fc9fa974a2095d0643c475ed60e6cbb8471a
Mumtahin Farabi <mfarabi619@gmail.com>
2024-12-17 21:24:21 -0500

    chore(config/nix): update commands for improved environment management

--- shell.nix
+++ shell.nix
@@ -1,30 +1,44 @@
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.mkShell {
+let
+  installPnpmCommand = "curl -fsSL https://get.pnpm.io/install.sh | sh -";
+
+in pkgs.mkShell {
   name = "cuhacking-2025-dev-env";
 
   buildInputs = [
-    pkgs.pnpm
     pkgs.figlet
     pkgs.jp2a
     pkgs.git
     pkgs.gh
     pkgs.lazygit
     pkgs.zellij
-    pkgs.zsh
     pkgs.eza
+    pkgs.yazi
+    pkgs.zsh
   ];
 
   shellHook = ''
+     # Check if pnpm exists, if not, install it
+    if ! command -v pnpm >/dev/null 2>&1; then
+      echo "pnpm not found, installing..."
+      ${installPnpmCommand}
+      echo "Please re-run the Nix Shell"
+      exit
+    fi
+
     pnpm env use 22.11.0 --global
 
-    alias  l='eza -lh  --icons=auto' # long list
+    # Aliases
+    alias l='eza -lh --icons=auto'
     alias ls='eza --icons=auto --sort=name --group-directories-first'
-    alias ll='eza -lha --icons=auto --sort --group-directories-first' # long list all
-    alias ld='eza -lhD --icons=auto' # long list dirs
-    alias lt='eza --icons=auto --tree' # list folder as tree
+    alias ll='eza -lha --icons=auto --sort --group-directories-first'
+    alias ld='eza -lhD --icons=auto'
+    alias lt='eza --icons=auto --tree'
+    alias y='yazi'
+    alias mkdir='mkdir -p'
 
+    # Display jp2a image
     jp2a --color ./libs/shared/assets/logos/cuHacking/logo-icon-wordmark-gradient-green.png
-
   '';
 }
