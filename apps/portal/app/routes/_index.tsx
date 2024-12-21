c3f89074dc06a8ed8c2aa734735e20bc47e7dfc5
Mumtahin Farabi <mfarabi619@gmail.com>
2024-12-21 02:07:45 -0500

    feat(ui/portal,api/cms): create and render social links collection

--- apps/portal/app/routes/_index.tsx
+++ apps/portal/app/routes/_index.tsx
@@ -1,7 +1,24 @@
+import { useLoaderData } from '@remix-run/react'
+
+export async function loader() {
+  const response = await fetch('http://localhost:3001/social-links')
+  const { docs } = await response.json()
+
+  const links = docs.map(link => link.platform || link.text)
+  return Response.json(links)
+}
+
 export default function Index() {
+  const links = useLoaderData()
+
   return (
-    <>
-      Hellooooo
-    </>
+    <div>
+      <h1>Social Links</h1>
+      <ul>
+        {links.map((text, index) => (
+          <li key={index}>{text}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
