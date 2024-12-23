8ce851d322b639a728338987a41636ca55bb1e1b
Mumtahin Farabi <mfarabi619@gmail.com>
2024-12-23 04:30:12 -0500

    feat(api): scaffold project

---
+++ apps/api/webpack.config.cjs
@@ -0,0 +1,20 @@
+const { join } = require('node:path')
+const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin')
+
+module.exports = {
+  output: {
+    path: join(__dirname, '../../dist/apps/api'),
+  },
+  plugins: [
+    new NxAppWebpackPlugin({
+      target: 'node',
+      compiler: 'tsc',
+      main: './src/main.ts',
+      tsConfig: './tsconfig.app.json',
+      assets: ['./src/assets'],
+      optimization: false,
+      outputHashing: 'none',
+      generatePackageJson: true,
+    }),
+  ],
+}
