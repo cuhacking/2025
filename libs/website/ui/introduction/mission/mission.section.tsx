9d495ca9f7d29b3e101f8f4d42d5da1290ed83ee
HasithDeAlwis <hasithde24@gmail.com>
2025-01-01 17:58:30 -0500

    feat(web/spline): change spline links

--- libs/website/ui/introduction/mission/mission.section.tsx
+++ libs/website/ui/introduction/mission/mission.section.tsx
@@ -1,3 +1,4 @@
+import BgKeyCaps from '@website/assets/ui/introduction/bg-keycap-1.webp'
 import { SplineComponent } from '@website/shared/ui/spline/spline-component'
 import { MISSION_CONSTANTS } from '../constants/mission.constants'
 import { Mission } from './mission'
@@ -6,8 +7,9 @@ export function MissionSection() {
   return (
     <section id="#about" className="relative flex justify-center w-full">
       <SplineComponent
-        className="absolute -bottom-[15vh] lg:-bottom-[5vh] left-0 scale-1 md:scale-[1.2]"
-        link="https://prod.spline.design/TGlqj05806lq8PRV/scene.splinecode"
+        className="absolute bottom-[14vh] lg:-bottom-[5vh] left-0 scale-1 md:scale-[1.2]"
+        link="https://prod.spline.design/9iGB68CM6hGlQYOQ/scene.splinecode"
+        imgSrc={BgKeyCaps}
       />
       <main className="max-w-screen-xl px-5 py-5 lg:px-20 lg:py-14">
         <div
