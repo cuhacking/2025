f51613fc8f0846069b3e18981706fd3350ee852f
HasithDeAlwis <hasithde24@gmail.com>
2025-01-05 18:27:48 -0500

    chore(deps): remove react-three

--- libs/website/ui/introduction/welcome/welcome.section.tsx
+++ libs/website/ui/introduction/welcome/welcome.section.tsx
@@ -1,42 +1,13 @@
 import BgKeyboard from '@website/assets/ui/introduction/bg-keyboard-1.webp'
 import { SplineComponent } from '@website/shared/ui/spline/spline-component'
-/* import type { ThreeElements } from '@react-three/fiber' */
-/* import { useFrame } from '@react-three/fiber' */
 import React from 'react'
 import { WELCOME_CONSTANTS } from '../constants/welcome.constants'
 import { Welcome } from './welcome'
 
-/* function Box(props: ThreeElements['mesh']) {
- *   const meshRef = useRef<THREE.Mesh>(null!)
- *   const [hovered, setHover] = useState(false)
- *   const [active, setActive] = useState(false)
- *   useFrame((state, delta) => (meshRef.current.rotation.x += delta)
- *   return (
- *     <mesh
- *       {...props}
- *       ref={meshRef}
- *       scale={active ? 1.5 : 1}
- *       onClick={event => setActive(!active)}
- *       onPointerOver={event => setHover(true)}
- *       onPointerOut={event => setHover(false)}
- *     >
- *       <boxGeometry args={[1, 1, 1]} />
- *       <meshStandardMaterial color={hovered ? 'hotpink' : '#2f74c0'} />
- *     </mesh>
- *   )
- * } */
-
 export function WelcomeSection() {
   return (
     <section className="relative flex justify-center w-full mx-auto lg:mx-0">
-      {/* <Canvas>
-        <ambientLight intensity={Math.PI / 2} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
-      </Canvas>
- */}
+
       <SplineComponent
         imgSrc={BgKeyboard}
         className="absolute -bottom-24 md:bottom-8 scale:-[0.85] lg:left-0 lg:top-0 lg:scale-[1.2]"
