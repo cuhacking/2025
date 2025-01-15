85612907b1760cebbaeceed4d68dfb0db8068a78
JeremyFriesenGitHub <jeremyfriesen@cmail.carleton.ca>
2025-01-15 15:50:21 -0500

    refactor(libs/docs): update feature component
    refactor(libs/docs): update feature storybook

--- libs/docs/components/ui/feature.stories.tsx
+++ libs/docs/components/ui/feature.stories.tsx
@@ -1,4 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { User } from 'lucide-react'
+import React from 'react'
 import { FUIFeatureSectionWithCards } from './feature'
 
 const meta: Meta<typeof FUIFeatureSectionWithCards> = {
@@ -8,8 +10,16 @@ const meta: Meta<typeof FUIFeatureSectionWithCards> = {
   },
   component: FUIFeatureSectionWithCards,
   tags: ['autodocs'],
-  args: {},
   argTypes: {
+    title: {
+      control: { type: 'text' },
+    },
+    description: {
+      control: { type: 'text' },
+    },
+    features: {
+      control: { type: 'object' },
+    },
   },
 }
 
@@ -18,6 +28,25 @@ export default meta
 type Story = StoryObj<typeof FUIFeatureSectionWithCards>
 export const UserExperience: Story = {
   args: {
+    title: 'User Experience',
+    description: 'The platform should reliably meet the needs of hackers, judges, sponsors, and organizers.',
+    features: [
+      {
+        title: 'Hackers',
+        desc: 'Hackers should be able to easily navigate the platform, find the information they need, and submit their projects.',
+        icon: <User />,
+      },
+      {
+        title: 'Judges',
+        desc: 'Judges should be able to easily view and evaluate projects, and provide feedback to hackers.',
+        icon: <User />,
+      },
+      {
+        title: 'Sponsors',
+        desc: 'Sponsors should be able to easily view and support projects, and engage with hackers.',
+        icon: <User />,
+      },
+    ],
   },
   render: args => (
     <div>
