d9874d75c7458d8e55c94c53fbe2f6a4224809f7
HasithDeAlwis <hasithde24@gmail.com>
2025-01-23 20:49:50 -0500

    feat(db): add cloud storage

--- libs/db/collections/Media.ts
+++ libs/db/collections/Media.ts
@@ -2,12 +2,6 @@ import type { CollectionConfig } from 'payload'
 
 export const Media: CollectionConfig = {
   slug: 'media',
-  access: {
-    read: () => true,
-    create: () => true,
-    update: () => true,
-    delete: () => true,
-  },
   fields: [
     {
       name: 'alt',
