973bdde3c95727ec24da4b62ae8b8c6e4bfbc782
HasithDeAlwis <hasithde24@gmail.com>
2025-01-28 22:05:25 -0500

    feat(web/footer): add powerd by netlify badge

--- libs/website/shared/ui/navigation/footer/ui/footer.presenter.tsx
+++ libs/website/shared/ui/navigation/footer/ui/footer.presenter.tsx
@@ -1,7 +1,7 @@
 import type { Media } from '@cuhacking/shared/types'
+import netlify_logo from '@cuhacking/shared/assets/logos/sponsors/netlify-white.svg'
 import { Link } from '@remix-run/react'
 import { Socials } from '@website/shared/ui/socials'
-import React from 'react'
 
 interface FooterProps {
   logo: string
@@ -10,23 +10,33 @@ interface FooterProps {
     media: Media
   }[]
 }
-
 export function FooterPresenter({ logo, socials }: FooterProps) {
   return (
-    <footer className="max-w-screen-xl px-4 mx-auto pt-5 pb-3.5 gap-y-6 flex flex-col lg:flex-row justify-center lg:justify-between">
-      <div className="flex flex-row items-center justify-center gap-2">
-        <Link to="/" aria-label="Return to homepage">
-          <img
-            src={logo}
-            alt="cuHacking logo"
-            className="transition-transform duration-300 hover:scale-[1.2]"
-          />
-        </Link>
-        <h2 className="text-transparent bg-greendiant bg-clip-text font-extrabold text-[34px]">
-          cuHacking
-        </h2>
+    <footer className="max-w-screen-xl px-4 mx-auto pt-5 pb-3.5 gap-y-6 flex flex-col justify-center lg:justify-between">
+      <div className="flex flex-col lg:flex-row gap-y-3 lg:justify-between">
+        <div className="flex flex-row items-center justify-center gap-2">
+          <Link to="/" aria-label="Return to homepage">
+            <img
+              src={logo}
+              alt="cuHacking logo"
+              className="transition-transform duration-300 hover:scale-[1.2]"
+            />
+          </Link>
+          <h2 className="text-transparent bg-greendiant bg-clip-text font-extrabold text-[34px]">
+            cuHacking
+          </h2>
+        </div>
+        <Socials socials={socials} className="justify-center" />
       </div>
-      <Socials socials={socials} className="justify-center" />
+      <div className="flex items-center flex-col lg:flex-row justify-between gap-x-3">
+        <a href="https://github.com/cuhacking/2025/graphs/contributors" rel="noopener noreferrer" className="hover:scale-110 duration-300 transition text-yellow-500 hover:text-accent">
+          Made with ❤️ for Hackers by Hackers.
+        </a>
+        <a href="https://netlify.com" target="_blank" rel="noopener noreferrer" className="flex hover:scale-110 duration-300 transition text-cyan-300 hover:text-accent items-center gap-x-3">
+          Powered by
+          <img className="w-24" src={netlify_logo} />
+        </a>
+      </div>
     </footer>
   )
 }
--- libs/website/ui/introduction/welcome/welcome.tsx
+++ libs/website/ui/introduction/welcome/welcome.tsx
@@ -1,76 +1,84 @@
 import { GlassmorphicCard } from '@cuhacking/shared/ui/glassmorphic-card'
 import { TerminalText } from '@cuhacking/shared/ui/terminal-text'
 import { Socials } from '@website/shared/ui/socials'
-import { useEffect, useState } from 'react'
+/* import { useEffect, useState } from 'react' */
 
-interface WelcomeData {
-  title: string
-  organization: string
-  date: string
-  callToAction: string
-}
+/* interface WelcomeData {
+*   title: string
+*   organization: string
+*   date: string
+*   callToAction: string
+* }
+*
+* interface Media {
+*   src: string
+*   alt: string
+* }
+*
 
-interface Media {
-  src: string
-  alt: string
-}
+*
+* async function fetchWelcomeData(): Promise<WelcomeData | null> {
+*   const query = `
+*     query {
+*       Welcomes {
+*         docs {
+*           title
+*           organization
+*           date
+*           callToAction
+*         }
+*       }
+*     }
+*   `
+*
+*   try {
+*     const response = await fetch(`${import.meta.env.VITE_API_URL}/api/graphql`, {
+*       method: 'POST',
+*       headers: {
+*         'Content-Type': 'application/json',
+*       },
+*       body: JSON.stringify({ query }),
+*     })
+*
+*     if (!response.ok) {
+*       throw new Error(`HTTP error! status: ${response.status}`)
+*     }
+*
+*     const data = await response.json()
+*     const welcomeData = data.data.Welcomes.docs[0] // Assuming only one document or taking the first one
+*     return {
+*       title: welcomeData.title,
+*       organization: welcomeData.organization,
+*       date: welcomeData.date,
+*       callToAction: welcomeData.callToAction,
+*     }
+*   }
+*   catch (error) {
+*     console.error('Failed to fetch welcome data:', error)
+*     return null
+*   }
+* } */
 
 interface IntroProps {
   socials: { link: string, media: Media, name: string }[]
 }
-
-async function fetchWelcomeData(): Promise<WelcomeData | null> {
-  const query = `
-    query {
-      Welcomes {
-        docs {
-          title
-          organization
-          date
-          callToAction
-        }
-      }
-    }
-  `
-
-  try {
-    const response = await fetch(`${import.meta.env.VITE_API_URL}/api/graphql`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ query }),
-    })
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`)
-    }
-
-    const data = await response.json()
-    const welcomeData = data.data.Welcomes.docs[0] // Assuming only one document or taking the first one
-    return {
-      title: welcomeData.title,
-      organization: welcomeData.organization,
-      date: welcomeData.date,
-      callToAction: welcomeData.callToAction,
-    }
-  }
-  catch (error) {
-    console.error('Failed to fetch welcome data:', error)
-    return null
-  }
-}
-
 export function Welcome({ socials }: IntroProps) {
-  const [welcomeData, setWelcomeData] = useState<WelcomeData | null>(null)
+  /* const [welcomeData, setWelcomeData] = useState<WelcomeData | null>(null) */
 
-  useEffect(() => {
-    async function getData() {
-      const data = await fetchWelcomeData()
-      setWelcomeData(data)
-    }
-    getData()
-  }, [])
+  /* useEffect(() => {
+*   async function getData() {
+*     const data = await fetchWelcomeData()
+*     setWelcomeData(data)
+*   }
+*   getData()
+* }, [])
+ */
+  const welcomeData = {
+    title: 'Hello, World',
+    organization: 'cuHacking',
+    date: 'Mar. 14 - 16 2025',
+    callToAction: 'In the meantime, check out all the cool stuff we\'re working on!',
+  }
 
   return (
     welcomeData
