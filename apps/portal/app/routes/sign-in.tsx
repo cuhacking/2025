564a92f782495b34e6c28d1eed4b24a4a4f45fcd
HasithDeAlwis <hasithde24@gmail.com>
2025-01-31 15:17:31 -0500

    fix(portal): unresolved module import for background image

--- apps/portal/app/routes/sign-in.tsx
+++ apps/portal/app/routes/sign-in.tsx
@@ -2,8 +2,6 @@ import { Signin as SigninPage } from '@cuhacking/portal/pages/signin/index'
 
 export default function Signin() {
   return (
-    <div>
-      <SigninPage />
-    </div>
+    <SigninPage />
   )
 }
