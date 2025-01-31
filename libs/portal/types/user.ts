80eeeecd8535fce407cf4c23f03ee5849217fdb5
anakafeel <hashmisaim037@gmail.com>
2025-01-31 14:35:34 -0500

    feat(portal): create profile questions

---
+++ libs/portal/types/user.ts
@@ -0,0 +1,44 @@
+import type { tShirtSizes } from './tShirt'
+
+export interface User {
+  profileStatus: UserProfileStatus
+  details: Partial<UserDetails>
+}
+
+export interface UserDetails {
+  firstName: string
+  lastName: string
+  avatar: string
+  preferredDisplayName: string
+  email: string
+  tShirtSize: tShirtSizes
+  age: number
+  isStudent: boolean
+  yearStanding?: number
+  expectedGraduationDate?: Date
+  degree?: string
+  institution?: string
+  fieldOfStudy?: string
+  phoneNumber: string
+  emergencyContactFirstName: string
+  emergencyContactLastName: string
+  emergencyContactPhoneNumber: string
+  emergencyContactRelationship: string
+  middleName?: string
+  gender: string
+  ethnicBackground?: string
+  dietaryRestrictions?: { label: string, value: string }[]
+  allergies?: { label: string, value: string }[]
+  discordHandle?: string
+  githubHandle?: string
+  instagramHandle?: string
+  behanceHandle?: string
+  website?: string
+  isPublicProfile?: boolean
+  isPublicResume?: boolean
+}
+
+export enum UserProfileStatus {
+  complete = 'complete',
+  notComplete = 'not_complete',
+}
