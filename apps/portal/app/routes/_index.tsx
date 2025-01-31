ba83e59dbf44fd7ce9ace7a89a1f5085fe0624fe
HasithDeAlwis <hasithde24@gmail.com>
2025-01-31 13:02:08 -0500

    feat(ui/portal): create signin page
    Note: not hooked up with BE

--- apps/portal/app/routes/_index.tsx
+++ apps/portal/app/routes/_index.tsx
@@ -1,22 +1,12 @@
-import { useLoaderData } from '@remix-run/react'
-
-export async function loader() {
-  const response = await fetch('http://localhost:3001/social-links')
-  const { docs } = await response.json()
-
-  const links = docs.map(link => link.platform || link.text)
-  return Response.json(links)
-}
-
 export default function Index() {
-  const links = useLoaderData()
+  const links = ['github']
 
   return (
     <div>
       <h1>Social Links</h1>
       <ul>
-        {links.map((text, index) => (
-          <li key={index}>{text}</li>
+        {links.map(text => (
+          <li key={text}>{text}</li>
         ))}
       </ul>
     </div>
--- libs/shared/ui/button/button.tsx
+++ libs/shared/ui/button/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline decoration-foreground',
+        primary: 'text-primary-foreground bg-primary hover:font-bold hover:bg-primary/90',
       },
       size: {
         default: 'h-10 px-4 py-2',
