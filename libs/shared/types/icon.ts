449ea496cd5000d98c0c480f8db98a9fe91e13b1
HasithDeAlwis <hasithde24@gmail.com>
2025-02-09 17:22:50 -0500

    feat(portal): create accordion header

---
+++ libs/shared/types/icon.ts
@@ -0,0 +1,9 @@
+export enum IconVariant {
+  profile = 'profile',
+  cellPhone = 'cell-phone',
+  shield = 'shield',
+  lock = 'lock',
+  warning = 'warning',
+  contactBook = 'contact-book',
+  education = 'education',
+}
