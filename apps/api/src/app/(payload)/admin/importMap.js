83676e08ebeb4f65fd7adee0d74a24ae9fe11041
Mumtahin Farabi <mfarabi619@gmail.com>
2025-02-09 15:20:53 -0500

    feat(ui/api/login): add custom components

--- apps/api/src/app/(payload)/admin/importMap.js
+++ apps/api/src/app/(payload)/admin/importMap.js
@@ -1,9 +1,13 @@
 import { Icon as Icon_618b1137dba3c4453c515b1f2fbf99f4 } from '../../../../src/components/icon'
 import { Logo as Logo_6453d4efeb33fd1ead5ac6bbffe223f0 } from '../../../../src/components/logo'
+import { AfterLogin as AfterLogin_bb88eaa5522dec17b63fae214366fe48 } from '../../../../src/components/after-login'
+import { BeforeLogin as BeforeLogin_f54cf4cfed7358d2461ebe4560290a2f } from '../../../../src/components/before-login'
 import { OAuth as OAuth_c34a0b2eff4436b5c63d31ebb7b6a3ea } from '../../../../src/components/oauth'
 
 export const importMap = {
   "/components/icon#Icon": Icon_618b1137dba3c4453c515b1f2fbf99f4,
   "/components/logo#Logo": Logo_6453d4efeb33fd1ead5ac6bbffe223f0,
+  "/components/after-login#AfterLogin": AfterLogin_bb88eaa5522dec17b63fae214366fe48,
+  "/components/before-login#BeforeLogin": BeforeLogin_f54cf4cfed7358d2461ebe4560290a2f,
   "/components/oauth#OAuth": OAuth_c34a0b2eff4436b5c63d31ebb7b6a3ea
 }
