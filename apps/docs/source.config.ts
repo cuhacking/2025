71fe7a1201fc22ab88b1a80f0a4bb1114399eae2
Mumtahin Farabi <mfarabi619@gmail.com>
2025-02-09 14:28:26 -0500

    chore(config/docs/source): add docgen

--- apps/docs/source.config.ts
+++ apps/docs/source.config.ts
@@ -1,4 +1,5 @@
 import { remarkMermaid } from '@theguild/remark-mermaid'
+import { fileGenerator, remarkDocGen } from 'fumadocs-docgen'
 import { defineConfig, defineDocs } from 'fumadocs-mdx/config'
 import rehypeKatex from 'rehype-katex'
 import remarkMath from 'remark-math'
@@ -9,7 +10,7 @@ export const { docs, meta } = defineDocs({
 
 export default defineConfig({
   mdxOptions: {
-    remarkPlugins: [remarkMath, remarkMermaid],
+    remarkPlugins: [remarkMath, remarkMermaid, [remarkDocGen, { generators: [fileGenerator()] }]],
     // Place it at first so that it won't be changed by syntax highlighter
     rehypePlugins: v => [rehypeKatex, ...v],
   },
