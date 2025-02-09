1c1b4039f68d59ada46c04ab9505959a3573a51e
HasithDeAlwis <hasithde24@gmail.com>
2025-02-09 17:10:26 -0500

    feat(shared): create input

---
+++ libs/shared/ui/input/input.stories.tsx
@@ -0,0 +1,35 @@
+import { Input } from './input'
+
+export default {
+  title: 'cuHacking Design System/Input',
+  component: Input,
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    type: {
+      control: 'text',
+      defaultValue: 'text',
+    },
+    className: {
+      control: 'text',
+      defaultValue: '',
+    },
+  },
+}
+
+const Template = (args: React.ComponentProps<typeof Input>) => <Input {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  type: 'text',
+  placeholder: 'Enter text...',
+  className: 'border-2 border-border p-2 rounded-md',
+}
+
+export const Number = Template.bind({})
+Number.args = {
+  type: 'number',
+  placeholder: 'Enter a number...',
+  className: 'border-2 border-border p-2 rounded-md',
+}
