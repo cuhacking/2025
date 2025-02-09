7aad07ec07a6533dfa22bebe82c44ac798c5c454
HasithDeAlwis <hasithde24@gmail.com>
2025-02-09 16:42:31 -0500

    feat(ui/storybook): add story for calendar component

---
+++ libs/external/shadcn/components/ui/calendar.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Calendar } from './calendar'
+
+const meta: Meta<typeof Calendar> = {
+  title: 'Shadcn-ui/Calendar',
+  component: Calendar,
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+    mode: 'single',
+  },
+}
+
+export default meta
+
+type Story = StoryObj<typeof Calendar>
+
+export const Default: Story = {}
+
+export const WithRangeSelection: Story = {
+  args: {
+    mode: 'range',
+  },
+}
