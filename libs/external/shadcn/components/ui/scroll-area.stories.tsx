94419c1bc0428df515c88969ffccad5fe1939e83
HasithDeAlwis <hasithde24@gmail.com>
2025-02-09 16:57:20 -0500

    feat(shadcn): create scroll-area

---
+++ libs/external/shadcn/components/ui/scroll-area.stories.tsx
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { ScrollArea } from './scroll-area'
+
+const meta: Meta<typeof ScrollArea> = {
+  title: 'Shadcn-ui/ScrollArea',
+  parameters: {
+    layout: 'centered',
+  },
+  component: ScrollArea,
+  argTypes: {
+    className: { control: 'text' },
+  },
+}
+
+export default meta
+
+type Story = StoryObj<typeof ScrollArea>
+
+export const VerticalScroll: Story = {
+  args: {
+    children: (
+      <div className="h-[300px] w-[200px] overflow-auto">
+        <div className="h-[500px] flex flex-col gap-3">
+          <p>Scroll down to see more content...</p>
+          <p>Scroll down to see more content...</p>
+          <p>Scroll down to see more content...</p>
+          <p>Scroll down to see more content...</p>
+          <p>Scroll down to see more content...</p>
+          <p>Scroll down to see more content...</p>
+          <p>Scroll down to see more content...</p>
+          <p>Scroll down to see more content...</p>
+        </div>
+      </div>
+    ),
+  },
+}
+
+export const HorizontalScroll: Story = {
+  args: {
+    children: (
+      <div className="w-[300px] overflow-auto">
+        <div className="w-[500px] flex gap-3">
+          <p>Scroll left to see more content...</p>
+          <p>Scroll left to see more content...</p>
+          <p>Scroll left to see more content...</p>
+          <p>Scroll left to see more content...</p>
+        </div>
+      </div>
+    ),
+  },
+}
