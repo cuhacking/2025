e298c30b33427dc0fefa4b90ddb7702fc3f6530e
Mumtahin Farabi <mfarabi619@gmail.com>
2025-02-09 23:46:14 -0500

    refactor(config/eslint): move antfu rules to shared lib

--- libs/shared/config/eslint/antfu.js
+++ libs/shared/config/eslint/antfu.js
@@ -3,8 +3,50 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   formatters: true,
   // unocss: true,
+  vue: true,
   react: true,
-  slidev: true,
-},
-// {}
-)
+  jsonc: true,
+  yaml: true,
+}, {
+  ignores: [
+    '**/dist',
+    'scaffold-examples/',
+    // TODO: remove after monorepo integration is complete
+    'apps/axiom/',
+    'libs/db/schema.ts',
+    'libs/cms/',
+    'apps/slides/slides.md',
+  ],
+})
+
+// export default [
+//   ...nx.configs['flat/base'],
+//   ...nx.configs['flat/typescript'],
+//   ...nx.configs['flat/javascript'],
+//   {
+//     ignores: ['**/dist'],
+//   },
+//   {
+//     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+//     rules: {
+//       '@nx/enforce-module-boundaries': [
+//         'error',
+//         {
+//           enforceBuildableLibDependency: true,
+//           allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
+//           depConstraints: [
+//             {
+//               sourceTag: '*',
+//               onlyDependOnLibsWithTags: ['*'],
+//             },
+//           ],
+//         },
+//       ],
+//     },
+//   },
+//   {
+//     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+//     // Override or add rules here
+//     rules: {},
+//   },
+// ];
