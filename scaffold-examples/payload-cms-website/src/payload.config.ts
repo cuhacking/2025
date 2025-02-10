3848acf3ed8fbd209187845ac502c5683c8fed6d
Mumtahin Farabi <mfarabi619@gmail.com>
2025-02-09 21:59:04 -0500

    chore(config/scaffold-examples/payload-cms-website): update docker compose for better devex

--- scaffold-examples/payload-cms-website/src/payload.config.ts
+++ scaffold-examples/payload-cms-website/src/payload.config.ts
@@ -1,4 +1,4 @@
-import { s3Storage } from '@payloadcms/storage-s3'
+// import { s3Storage } from '@payloadcms/storage-s3'
 import { postgresAdapter } from '@payloadcms/db-postgres'
 
 import sharp from 'sharp' // sharp-import
@@ -69,21 +69,21 @@ export default buildConfig({
   globals: [Header, Footer],
   plugins: [
     ...plugins,
-    s3Storage({
-      collections: {
-        media: true,
-      },
-      bucket: process.env.S3_BUCKET || '',
-      config: {
-        forcePathStyle: true, // Important for using Supabase
-        credentials: {
-          accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
-          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
-        },
-        region: process.env.S3_REGION || '',
-        endpoint: process.env.S3_ENDPOINT || '',
-      },
-    }),
+    // s3Storage({
+    //   collections: {
+    //     media: true,
+    //   },
+    //   bucket: process.env.S3_BUCKET || '',
+    //   config: {
+    //     forcePathStyle: true, // Important for using Supabase
+    //     credentials: {
+    //       accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
+    //       secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
+    //     },
+    //     region: process.env.S3_REGION || '',
+    //     endpoint: process.env.S3_ENDPOINT || '',
+    //   },
+    // }),
   ],
   secret: process.env.PAYLOAD_SECRET,
   sharp,
