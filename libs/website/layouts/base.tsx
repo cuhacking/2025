6719185f6129589d657abcfe93ad7c2bd84b3fb0
Mumtahin Farabi <mfarabi619@gmail.com>
2025-02-09 19:28:10 -0500

    style: lint repo

--- libs/website/layouts/base.tsx
+++ libs/website/layouts/base.tsx
@@ -1,21 +1,21 @@
+import type { ReactNode } from 'react'
 import {
   FOOTER_CONSTANTS,
   FooterPresenter,
   NAVBAR_CONSTANTS,
   NavbarContainer,
 } from '@website/shared/ui/navigation'
-import { ReactNode } from 'react'
+
 export function Layout({ children }: { children: ReactNode }) {
   return (
     <>
-         <NavbarContainer
-            links={NAVBAR_CONSTANTS.LINKS}
-            logo={NAVBAR_CONSTANTS.LOGO}
-            socials={NAVBAR_CONSTANTS.SOCIALS}
-            hamburger={NAVBAR_CONSTANTS.HAMBURGER}
-            cross={NAVBAR_CONSTANTS.CROSS}
-          />
-
+      <NavbarContainer
+        links={NAVBAR_CONSTANTS.LINKS}
+        logo={NAVBAR_CONSTANTS.LOGO}
+        socials={NAVBAR_CONSTANTS.SOCIALS}
+        hamburger={NAVBAR_CONSTANTS.HAMBURGER}
+        cross={NAVBAR_CONSTANTS.CROSS}
+      />
 
       <div className="mt-10">{children}</div>
       <FooterPresenter
