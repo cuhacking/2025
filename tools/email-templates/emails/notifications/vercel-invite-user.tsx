059e10793a8b8c9728764b8696ad9fc51fc62d16
Mumtahin Farabi <mfarabi619@gmail.com>
2025-02-10 04:26:50 -0500

    feat(ui/email): add templates

--- tools/email-templates/emails/notifications/vercel-invite-user.tsx
+++ tools/email-templates/emails/notifications/vercel-invite-user.tsx
@@ -14,26 +14,26 @@ import {
   Section,
   Tailwind,
   Text,
-} from '@react-email/components'
-import * as React from 'react'
+} from '@react-email/components';
+import * as React from 'react';
 
 interface VercelInviteUserEmailProps {
-  username?: string
-  userImage?: string
-  invitedByUsername?: string
-  invitedByEmail?: string
-  teamName?: string
-  teamImage?: string
-  inviteLink?: string
-  inviteFromIp?: string
-  inviteFromLocation?: string
+  username?: string;
+  userImage?: string;
+  invitedByUsername?: string;
+  invitedByEmail?: string;
+  teamName?: string;
+  teamImage?: string;
+  inviteLink?: string;
+  inviteFromIp?: string;
+  inviteFromLocation?: string;
 }
 
 const baseUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : ''
+  : '';
 
-export function VercelInviteUserEmail({
+export const VercelInviteUserEmail = ({
   username,
   userImage,
   invitedByUsername,
@@ -43,8 +43,8 @@ export function VercelInviteUserEmail({
   inviteLink,
   inviteFromIp,
   inviteFromLocation,
-}: VercelInviteUserEmailProps) {
-  const previewText = `Join ${invitedByUsername} on Vercel`
+}: VercelInviteUserEmailProps) => {
+  const previewText = `Join ${invitedByUsername} on Vercel`;
 
   return (
     <Html>
@@ -63,38 +63,21 @@ export function VercelInviteUserEmail({
               />
             </Section>
             <Heading className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">
-              Join
-              {' '}
-              <strong>{teamName}</strong>
-              {' '}
-              on
-              {' '}
-              <strong>Vercel</strong>
+              Join <strong>{teamName}</strong> on <strong>Vercel</strong>
             </Heading>
             <Text className="text-black text-[14px] leading-[24px]">
-              Hello
-              {' '}
-              {username}
-              ,
+              Hello {username},
             </Text>
             <Text className="text-black text-[14px] leading-[24px]">
-              <strong>{invitedByUsername}</strong>
-              {' '}
-              (
+              <strong>{invitedByUsername}</strong> (
               <Link
                 href={`mailto:${invitedByEmail}`}
                 className="text-blue-600 no-underline"
               >
                 {invitedByEmail}
               </Link>
-              ) has invited you to the
-              {' '}
-              <strong>{teamName}</strong>
-              {' '}
-              team on
-              {' '}
-              <strong>Vercel</strong>
-              .
+              ) has invited you to the <strong>{teamName}</strong> team on{' '}
+              <strong>Vercel</strong>.
             </Text>
             <Section>
               <Row>
@@ -133,25 +116,18 @@ export function VercelInviteUserEmail({
               </Button>
             </Section>
             <Text className="text-black text-[14px] leading-[24px]">
-              or copy and paste this URL into your browser:
-              {' '}
+              or copy and paste this URL into your browser:{' '}
               <Link href={inviteLink} className="text-blue-600 no-underline">
                 {inviteLink}
               </Link>
             </Text>
             <Hr className="border border-solid border-[#eaeaea] my-[26px] mx-0 w-full" />
             <Text className="text-[#666666] text-[12px] leading-[24px]">
-              This invitation was intended for
-              {' '}
-              <span className="text-black">{username}</span>
-              . This invite was
-              sent from
-              <span className="text-black">{inviteFromIp}</span>
-              {' '}
-              located in
-              {' '}
-              <span className="text-black">{inviteFromLocation}</span>
-              . If you
+              This invitation was intended for{' '}
+              <span className="text-black">{username}</span>. This invite was
+              sent from <span className="text-black">{inviteFromIp}</span>{' '}
+              located in{' '}
+              <span className="text-black">{inviteFromLocation}</span>. If you
               were not expecting this invitation, you can ignore this email. If
               you are concerned about your account's safety, please reply to
               this email to get in touch with us.
@@ -160,8 +136,8 @@ export function VercelInviteUserEmail({
         </Body>
       </Tailwind>
     </Html>
-  )
-}
+  );
+};
 
 VercelInviteUserEmail.PreviewProps = {
   username: 'alanturing',
@@ -173,6 +149,6 @@ VercelInviteUserEmail.PreviewProps = {
   inviteLink: 'https://vercel.com/teams/invite/foo',
   inviteFromIp: '204.13.186.218',
   inviteFromLocation: 'São Paulo, Brazil',
-} as VercelInviteUserEmailProps
+} as VercelInviteUserEmailProps;
 
-export default VercelInviteUserEmail
+export default VercelInviteUserEmail;
