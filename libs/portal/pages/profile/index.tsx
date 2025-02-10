2b822a16b17dd10fb0a36b233f9cbbd67e7e5851
HasithDeAlwis <hasithde24@gmail.com>
2025-02-09 22:40:43 -0500

    feat(ui/portal): create /profile route

---
+++ libs/portal/pages/profile/index.tsx
@@ -0,0 +1,18 @@
+import type { User } from '@cuhacking/portal/entities/user';
+import { Header, Questions } from '@cuhacking/portal/features/profile';
+import { Toaster } from '@cuhacking/shared/ui/sonner';
+
+export function ProfilePage({ user }: { user: User }) {
+  return (
+    <div className="px-2.5 py-5">
+      <Toaster />
+      <Header
+        status={user.profileStatus}
+        firstName={user.details.firstName || ''}
+        lastName={user.details.lastName || ''}
+        avatarUrl={user.details.avatar || ''}
+      />
+      <Questions status={user.profileStatus} user={user.details} />
+    </div>
+  );
+}
