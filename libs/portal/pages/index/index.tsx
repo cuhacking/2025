122b3fce7b5cfd4637643550a064c4cc7ff4b61a
HasithDeAlwis <hasithde24@gmail.com>
2025-02-12 00:57:03 -0500

    refactor(portal): use hackathon application status enum

--- libs/portal/pages/index/index.tsx
+++ libs/portal/pages/index/index.tsx
@@ -1,3 +1,4 @@
+import { UserHackathonApplicationStatus } from '@cuhacking/portal/types/user'
 import { Banner } from '@cuhacking/portal/ui/dashboard/banner'
 import { HackathonCountdown } from '@cuhacking/portal/ui/dashboard/hackathon-countdown'
 import { Stat } from '@cuhacking/portal/ui/dashboard/stat'
@@ -10,15 +11,11 @@ const constants = {
     name: 'Saim',
   },
   hackathonDate: new Date(2025, 2, 14),
-  status: 'pending',
+  hackathonApplicationStatus: UserHackathonApplicationStatus.notComplete,
   stats: [
     {
-      label: 'Total Applications',
-      value: 1000,
-    },
-    {
       label: 'Money Raised',
-      value: '$12,230',
+      value: '$20 030',
     },
   ],
 }
@@ -30,18 +27,21 @@ export function Home() {
       <div className="flex flex-1 flex-col w-full p-4 gap-5">
         <Banner name={constants.user.name} />
         <div className="flex justify-center w-full">
-          <Button
-            className="text-black px-8 md:px-12 py-2.5"
-            variant="secondary"
-          >
-            APPLY TO CUHACKING 6
-          </Button>
+          {constants.hackathonApplicationStatus === UserHackathonApplicationStatus.notComplete
+          && (
+            <Button
+              className="text-black px-8 md:px-12 py-2.5"
+              variant="secondary"
+            >
+              APPLY TO CUHACKING 6
+            </Button>
+          )}
         </div>
         <HackathonCountdown date={constants.hackathonDate} />
         {constants.stats.map(stat => (
           <Stat key={stat.label} label={stat.label} value={stat.value} />
         ))}
-        <UserStatus name={constants.user.name} status={constants.status} />
+        { constants.hackathonApplicationStatus !== UserHackathonApplicationStatus.notComplete && <UserStatus name={constants.user.name} status={constants.status} /> }
       </div>
     </div>
   )
