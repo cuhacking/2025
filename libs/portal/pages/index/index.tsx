4bd714ba6c49b0a4486d132346cec58202544c5b
HasithDeAlwis <hasithde24@gmail.com>
2025-02-11 20:02:11 -0500

    feat(ui/portal): add dashboard

--- libs/portal/pages/index/index.tsx
+++ libs/portal/pages/index/index.tsx
@@ -2,12 +2,14 @@ import { Banner } from '@cuhacking/portal/ui/dashboard/banner'
 import { HackathonCountdown } from '@cuhacking/portal/ui/dashboard/hackathon-countdown'
 import { Stat } from '@cuhacking/portal/ui/dashboard/stat'
 import { UserStatus } from '@cuhacking/portal/ui/dashboard/user-status'
+import { Sidebar } from '@cuhacking/portal/ui/sidebar'
+import { Button } from '@cuhacking/shared/ui/button'
 
 const constants = {
   user: {
-    name: 'Hasith',
+    name: 'Saim',
   },
-  hackathonDate: new Date(2025, 3, 14),
+  hackathonDate: new Date(2025, 2, 14),
   status: 'pending',
   stats: [
     {
@@ -15,22 +17,32 @@ const constants = {
       value: 1000,
     },
     {
-      label: 'Money Raised', // Fixed key from 'name' to 'label'
+      label: 'Money Raised',
       value: '$12,230',
     },
   ],
 }
 
-export function Index() {
+export function Home() {
   return (
-    <div className="flex flex-col gap-5 relative w-full max-w-screen-xl mx-auto">
-      <Banner name={constants.user.name} />
-      <HackathonCountdown date={constants.hackathonDate} />
-      {constants.stats.map((stat, index) => (
-        <Stat key={index} label={stat.label} value={stat.value} />
-      ))}
-      <UserStatus name={constants.user.name} status={constants.status} />
-
+    <div className="flex h-screen">
+      <Sidebar />
+      <div className="flex flex-1 flex-col w-full p-4 gap-5">
+        <Banner name={constants.user.name} />
+        <div className="flex justify-center w-full">
+          <Button
+            className="text-black px-8 md:px-12 py-2.5"
+            variant="secondary"
+          >
+            APPLY TO CUHACKING 6
+          </Button>
+        </div>
+        <HackathonCountdown date={constants.hackathonDate} />
+        {constants.stats.map(stat => (
+          <Stat key={stat.label} label={stat.label} value={stat.value} />
+        ))}
+        <UserStatus name={constants.user.name} status={constants.status} />
+      </div>
     </div>
   )
 }
