aaab60d614a9313913b94f4b290fca25f071d526
HasithDeAlwis <hasithde24@gmail.com>
2025-02-12 00:55:54 -0500

    feat(ui/portal): adjust padding on home route

--- apps/portal/app/routes/_index.tsx
+++ apps/portal/app/routes/_index.tsx
@@ -1,13 +1,13 @@
 import dashboard_background from '@cuhacking/portal/assets/backgrounds/dashboard-bg-1.webp'
-import { Index as Home } from '@cuhacking/portal/pages/index/index'
+import { Home } from '@cuhacking/portal/pages/index/index'
 
 export default function Signin() {
   return (
-    <div className="py-20 px-5">
+    <div className="w-full">
       <Home />
       <img
         src={dashboard_background}
-        alt=""
+        alt="Background"
         aria-hidden="true"
         className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
       />
