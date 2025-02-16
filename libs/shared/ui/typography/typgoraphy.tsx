6bf84c8cd7d1d281c42810b675be431fa10b6125
HasithDeAlwis <hasithde24@gmail.com>
2025-02-16 11:35:51 -0500

    feat(ui/portal,web): modify h4 typography to text-xl on small screens

--- libs/shared/ui/typography/typgoraphy.tsx
+++ libs/shared/ui/typography/typgoraphy.tsx
@@ -23,7 +23,7 @@ const typographyVariants = cva('', {
       'h1': 'text-6xl font-bold',
       'h2': 'text-5xl font-normal',
       'h3': 'font-medium leading-10 text-4xl tracking-normal uppercase no-underline',
-      'h4': 'text-2xl font-normal',
+      'h4': 'text-xl sm:text-2xl font-normal',
       'h5': 'text-lg font-normal',
       'h6': 'font-medium leading-5 text-sm tracking-normal uppercase no-underline',
       'paragraph-base': 'text-base font-normal',
