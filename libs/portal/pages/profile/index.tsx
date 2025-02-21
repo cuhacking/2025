96301318fe292599d1b1884cecdc220a8a05c79c
Aashna-Verma <aashna.verma@outlook.com>
2025-02-20 20:36:57 -0500

    feat(ui/portal): add profile page background

--- libs/portal/pages/profile/index.tsx
+++ libs/portal/pages/profile/index.tsx
@@ -1,4 +1,6 @@
 import type { User } from '@cuhacking/portal/types/user'
+import dashboard_left from '@cuhacking/portal/assets/backgrounds/dashboard-bg-left.webp'
+import dashboard_right from '@cuhacking/portal/assets/backgrounds/dashboard-bg-right.webp'
 import { Header, Questions } from '@cuhacking/portal/features/profile'
 import { Toaster } from '@cuhacking/shared/ui/sonner'
 
@@ -6,6 +8,18 @@ export function ProfilePage({ user }: { user: User }) {
   return (
     <div className="px-2.5 py-5 m-auto max-w-screen-xl">
       <Toaster />
+      <div className="">
+        <img
+          src={dashboard_left}
+          alt=""
+          className="fixed top-0 left-0 w-auto h-full object-cover z-[-1]"
+        />
+        <img
+          src={dashboard_right}
+          alt=""
+          className="fixed top-0 right-0 w-auto h-full object-cover z-[-1]"
+        />
+      </div>
       <Header
         status={user.profileStatus}
         firstName={user.details.firstName || ''}
