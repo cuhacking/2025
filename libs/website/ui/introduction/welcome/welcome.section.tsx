44cdc05de1e9824a9beb4439a1ba165e1864fb68
HasithDeAlwis <hasithde24@gmail.com>
2025-02-22 09:21:12 -0500

    feat(ui/web): span hero bg-keyboward to the width of screen

--- libs/website/ui/introduction/welcome/welcome.section.tsx
+++ libs/website/ui/introduction/welcome/welcome.section.tsx
@@ -1,6 +1,4 @@
 import BgKeyboard from '@website/assets/ui/introduction/bg-keyboard-1.webp'
-import { Banner } from '@website/shared/ui/banner'
-import { NAVBAR_CONSTANTS } from '@website/shared/ui/navigation'
 import { SplineComponent } from '@website/shared/ui/spline/spline-component.client'
 import { ClientOnly } from 'remix-utils/client-only'
 import { WELCOME_CONSTANTS } from '../constants/welcome.constants'
@@ -8,19 +6,13 @@ import { Welcome } from './welcome'
 
 export function WelcomeSection() {
   return (
-    <section className="relative flex justify-center w-full mx-auto lg:mx-0">
-      <div className="absolute -top-2 -right-2 lg:top-0 lg:right-72 z-50 max-w-[100px] md:max-w-[120px] lg:max-w-[130px]">
-        <Banner
-          banner={NAVBAR_CONSTANTS.MLH_BANNER}
-          className="scale-[0.5] md:scale-[0.6] lg:scale-[0.9] transform-gpu"
-        />
-      </div>
+    <section className="flex justify-center w-full mx-auto lg:mx-0">
 
       <ClientOnly>
         {() => (
           <SplineComponent
             imgSrc={BgKeyboard}
-            className="absolute -bottom-24 md:bottom-8 lg:left-0 lg:top-0 scale-[0.85] lg:scale-[1.2]"
+            className="absolute -bottom-24 md:bottom-8 lg:left-0 lg:top-0 lg:scale-[1.2]"
             link={WELCOME_CONSTANTS.SPLINE_LINK}
           />
         )}
