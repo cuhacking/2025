b0ac97fb17e0f2bfe45e4f836144c3ed814307b5
Zesan <zesanrahim@gmail.com>
2025-02-23 18:30:06 -0500

    feat(ui/portal/legal): adjust styling

--- libs/portal/features/legal/ui/legal.tsx
+++ libs/portal/features/legal/ui/legal.tsx
@@ -12,11 +12,12 @@ export function Legal({ legalData }: LegalPageProps) {
   const [scrollEndReached, setScrollEndReached] = useState<Record<string, boolean>>({})
 
   const handleScroll = (value: string, e: React.UIEvent<HTMLElement>) => {
-    const scrollHeight = e.currentTarget.scrollHeight
-    const scrollTop = e.currentTarget.scrollTop
-    const clientHeight = e.currentTarget.clientHeight
+    const target = e.currentTarget
+    const scrollHeight = target.scrollHeight
+    const scrollTop = target.scrollTop
+    const clientHeight = target.clientHeight
 
-    if (scrollTop + clientHeight >= scrollHeight) {
+    if (scrollTop + clientHeight >= scrollHeight - 2) {
       setScrollEndReached(prev => ({
         ...prev,
         [value]: true,
@@ -35,25 +36,29 @@ export function Legal({ legalData }: LegalPageProps) {
   const allScrolled = legalData.every(({ value }) => scrollEndReached[value])
 
   return (
-    <div className="max-w-screen-lg mx-auto flex flex-col">
-      <div className="flex flex-col mb-4">
-        <h3 className="text-lg font-bold">Legalities</h3>
-        <p className="text-sm text-gray-400">
-          <Typography variant="paragraph-base">YO! Before we get into it, read these please.</Typography>
-        </p>
-        <p className="text-sm text-gray-400">
-          <Typography variant="paragraph-base">!! MUST READ ALL !!</Typography>
-        </p>
+    <div className="max-w-screen-xl px-4 sm:px-6 md:px-8 mx-auto flex flex-col py-4">
+      <div className="flex flex-col py-4 gap-y-1.5 ">
+        {/* <h3 className="text-base sm:text-lg font-bold mb-3">Legalities</h3> */}
+        <Typography variant="h4" className="">Legalities</Typography>
+        <Typography variant="paragraph-xs" className="text-gray-400">YO! Before we get into it, read these please.</Typography>
+        <Typography variant="paragraph-xs" className="text-gray-400">!! MUST READ ALL !!</Typography>
       </div>
-      <Accordion type="single" collapsible className="w-full h-auto">
+
+      <Accordion type="single" collapsible className="flex flex-col gap-4 w-full">
         {legalData.map(({ value, title, content, buttonContent }) => (
-          <AccordionItem key={value} value={value} className="border-b border-white">
-            <AccordionTrigger className="text-center text-primary">{title}</AccordionTrigger>
-            <AccordionContent className="flex flex-col gap-y-2">
-              <ScrollArea className="max-h-60 w-auto rounded-md border p-2 overflow-y-auto" onScroll={e => handleScroll(value, e)}>
+          <AccordionItem key={value} value={value} className="border-b border-white w-full">
+            <AccordionTrigger className="pt-3 pb-1.5 flex justify-between items-center w-full hover:no-underline">
+              <Typography variant="h4" className="text-primary">{title}</Typography>
+            </AccordionTrigger>
+
+            <AccordionContent className="flex flex-col gap-4">
+              <ScrollArea
+                className="max-h-40 sm:max-h-60 rounded-md border p-4 overflow-y-auto bg-neutral-900"
+                onScroll={e => handleScroll(value, e)}
+              >
                 {content}
               </ScrollArea>
-              <div className="flex gap-x-3">
+              <div className="flex flex-row gap-3">
                 <Checkbox
                   className={cn(
                     'w-4 h-4',
@@ -61,31 +66,33 @@ export function Legal({ legalData }: LegalPageProps) {
                   )}
                   disabled={!scrollEndReached[value]}
                   onCheckedChange={() => handleCheckboxChange(value)}
-
                 />
-
-                <Typography variant="paragraph-base">
+                <Typography variant="paragraph-base" className="text-left">
                   {buttonContent}
+                  {' '}
+                  <span className="text-red-500">*</span>
                 </Typography>
               </div>
-
             </AccordionContent>
           </AccordionItem>
         ))}
       </Accordion>
-      <Button
-        onClick={() => {}}
-        disabled={!allChecked || !allScrolled}
-        className={cn(
-          'mt-4 p-3 rounded-lg text-white',
-          {
-            'bg-primary opacity-50 cursor-not-allowed': !allChecked || !allScrolled,
-            'bg-primary': allChecked && allScrolled,
-          },
-        )}
-      >
-        <Typography variant="h6">LETS CREATE YOUR PROFILE</Typography>
-      </Button>
+
+      <div className="px-4 flex justify-center pb-6">
+        <Button
+          onClick={() => { }}
+          disabled={!allChecked || !allScrolled}
+          className={cn(
+            'mt-4 p-3 rounded-lg text-black w-full sm:max-w-xs h-[36px] flex items-center',
+            {
+              'bg-primary opacity-50 cursor-not-allowed': !allChecked || !allScrolled,
+              'bg-primary': allChecked && allScrolled,
+            },
+          )}
+        >
+          <Typography variant="h6">LETS CREATE YOUR PROFILE</Typography>
+        </Button>
+      </div>
     </div>
   )
 }
