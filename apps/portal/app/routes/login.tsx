89a432087e168ab016922da9f468d83823f358d4
HasithDeAlwis <hasithde24@gmail.com>
2025-02-25 17:01:29 -0500

    refactor(portal/login): use redirect with action function instead of navigate

--- apps/portal/app/routes/login.tsx
+++ apps/portal/app/routes/login.tsx
@@ -1,4 +1,25 @@
+import { userFlowActor } from '@/engine/actors/user'
 import { Login as LoginPage } from '@cuhacking/portal/pages/login'
+import { redirect } from '@remix-run/node'
+
+export function action() {
+  // do LinkedIn auth validation
+  // then redirect to dashboard
+  // update the user entry with the LinkedIn information
+
+  try {
+    /* setIsLoading(true) */
+    userFlowActor.send({ type: 'LOGIN_SUCCESS' })
+
+    return redirect('/terms')
+  }
+  catch (error) {
+    console.error('Login failed:', error)
+  }
+  finally {
+    /* setIsLoading(false) */
+  }
+}
 
 export default function Login() {
   return (
