f698138f4a3da19ecf1c27e5654ff0b1f345293e
Mumtahin Farabi <mfarabi619@gmail.com>
2025-03-02 16:44:49 -0500

    test(axiom-e2e): create project

---
+++ apps/axiom-e2e/src/index.spec.ts
@@ -0,0 +1,59 @@
+import { Ensure, equals } from '@serenity-js/assertions'
+import { Task } from '@serenity-js/core'
+import { describe, it } from '@serenity-js/playwright-test'
+import { Navigate, Page } from '@serenity-js/web'
+
+export function startLoggedOut() {
+  return Task.where('#actor starts logged out', Navigate.to('/'), Ensure.that(
+    Page.current().title().describedAs('Login -cuHacking 2025'),
+    equals('Dashboard - cuHacking Portal'),
+  ))
+}
+
+describe('Hacker', () => {
+  it('should be able to log in with LinkedIn', async ({ actor }) => {
+    await actor.attemptsTo(
+    )
+  })
+
+  it.skip('should be able to log in with GitHub', async ({ actor }) => {
+    await actor.attemptsTo(
+    )
+  })
+
+  it('should see an account created if one doesn\'t already exist', async ({ actor }) => {
+    await actor.attemptsTo(
+
+    )
+  })
+
+  it('should see account details filled in for LinkedIn', async ({ actor }) => {
+    await actor.attemptsTo(
+    )
+  })
+
+  it('should be able to logout', async ({ actor }) => {
+    await actor.attemptsTo(
+    )
+  })
+})
+
+// it.skip('should be able to log in with Discord', async ({ actor }) => {
+//   await actor.attemptsTo(
+//   )
+// })
+
+// it.skip('should see account details filled in for GitHub', async ({ actor }) => {
+//   await actor.attemptsTo(
+//   )
+// })
+
+// it('should be able to log in with Google', async ({ actor }) => {
+//   await actor.attemptsTo(
+//   )
+// })
+
+// it('should see account details filled in for Google', async ({ actor }) => {
+//   await actor.attemptsTo(
+//   )
+// })
