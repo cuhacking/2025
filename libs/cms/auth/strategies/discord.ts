65ae78735939725c90e1b0cfaddde4afe2e64f01
Mumtahin Farabi <mfarabi619@gmail.com>
2025-03-02 23:48:04 -0500

    refactor(cms,db): improve auth structure and identity linking

--- libs/cms/auth/strategies/discord.ts
+++ libs/cms/auth/strategies/discord.ts
@@ -1,11 +1,9 @@
 // https://discord.com/developers/docs/topics/oauth2
 import { PayloadRequest } from "payload";
 import { OAuth2Plugin, defaultGetToken } from "payload-oauth2";
-import { baseConfig, discordStrategyConfig } from "@/cms/endpoints/auth/config"
+import { discordStrategyConfig } from "@/cms/auth/config"
 
 export const discordOAuth = OAuth2Plugin({
-  ...baseConfig,
-  ...discordStrategyConfig,
   useEmailAsIdentity: true,
   scopes: [
     "identify",
@@ -20,7 +18,7 @@ export const discordOAuth = OAuth2Plugin({
     const user = await response.json();
 
     return {
-      email: user.email,
+      // email: user.email,
       // firstName: user.given_name,
       // lastName: user.family_name,
       // displayName: user.name,
@@ -28,11 +26,5 @@ export const discordOAuth = OAuth2Plugin({
       // discordSub: user.sub,
     };
   },
-  successRedirect: (req: PayloadRequest, accessToken?: string) => {
-    return "/admin";
-  },
-  failureRedirect: (req, err) => {
-    req.payload.logger.error(err);
-    return "/admin/login";
-  },
+  ...discordStrategyConfig,
 });
