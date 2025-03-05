61cb9b79352244ce9dcc5dec1f15cad4b3035d14
Mumtahin Farabi <mfarabi619@gmail.com>
2025-03-05 10:10:23 +0000

    feat(axiom): redirect home route to portal login

--- apps/axiom/src/app/(payload)/page.tsx
+++ apps/axiom/src/app/(payload)/page.tsx
@@ -1,5 +1,32 @@
 import { redirect } from 'next/navigation'
+import { cookies } from 'next/headers'
+import { getPayload } from 'payload'
+import config from '@payload-config'
 
-export default function Home() {
-  redirect('/admin')
+export default async function Home() {
+  const payload = await getPayload({ config })
+
+  const token = await cookies()
+
+if (!token.get('payload-token')?.value) {
+  return redirect(
+    process.env.NODE_ENV === 'development'
+      ? process.env.CUHACKING_2025_PORTAL_LOCAL_URL
+      : process.env.CUHACKING_2025_PORTAL_PUBLIC_URL
+  )
+}
+
+  try {
+    const { user } = await payload.auth.verifyJWT(token)
+
+    if (user) {
+      redirect('/admin')
+    }
+  } catch (error) {
+    console.error('Auth error:', error)
+    redirect(
+    process.env.NODE_ENV === 'development'
+      ? process.env.CUHACKING_2025_PORTAL_LOCAL_URL
+    : process.env.CUHACKING_2025_PORTAL_PUBLIC_URL)
+  }
 }
