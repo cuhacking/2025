c2867d487bb5d0e620c7714e99cf7fe1712d0ed5
HasithDeAlwis <hasithde24@gmail.com>
2025-03-08 16:35:12 +0000

    feat(portal): save userId in session after successful login

--- apps/portal/app/routes/login.tsx
+++ apps/portal/app/routes/login.tsx
@@ -2,12 +2,13 @@
 import type { LoaderFunction } from '@remix-run/node'
 import { Login as LoginPage } from '@cuhacking/portal/pages/login'
 import { redirect } from '@remix-run/node'
+import { commitSession, getSession } from '../sessions'
 
 export const loader: LoaderFunction = async ({ request }) => {
   const cookie = request.headers.get('Cookie')
 
   try {
-    const res = await fetch(`${process.env.NODE_ENV === 'development' ? process.env.CUHACKING_2025_PORTAL_LOCAL_URL : process.env.CUHACKING_2025_PORTAL_PUBLIC_URL}/api/users/me`, {
+    const res = await fetch(`${process.env.NODE_ENV === 'development' ? process.env.CUHACKING_2025_AXIOM_LOCAL_URL : process.env.CUHACKING_2025_AXIOM_PUBLIC_URL}/api/users/me`, {
       headers: { Cookie: cookie || '' },
     })
 
@@ -17,8 +18,16 @@ export const loader: LoaderFunction = async ({ request }) => {
 
     const data = await res.json()
 
+    const session = await getSession(cookie)
+    session.set('userId', data.user.id)
+    const newCookie = await commitSession(session)
+
     if (data?.user) {
-      return redirect('/dashboard')
+      return redirect('/', {
+        headers: {
+          'Set-Cookie': newCookie || '',
+        },
+      })
     }
   }
   catch (error) {
