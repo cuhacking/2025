8d58be14c33a8d1900609b09b99bde67eaeed157
HasithDeAlwis <hasithde24@gmail.com>
2025-03-08 23:44:25 +0000

    feat(portal/dashbaord): center welcome message

--- libs/portal/ui/dashboard/user-status/user-status.tsx
+++ libs/portal/ui/dashboard/user-status/user-status.tsx
@@ -1,76 +1,36 @@
 import arrow_icon from '@cuhacking/shared/assets/icons/general/arrow-1.svg'
-import { Button } from '@cuhacking/shared/ui/button'
 import { GlassmorphicCard } from '@cuhacking/shared/ui/glassmorphic-card'
 import { TerminalText } from '@cuhacking/shared/ui/terminal-text/terminal-text'
 import { Typography } from '@cuhacking/shared/ui/typography'
 import { cn } from '@cuhacking/shared/utils/cn'
 
-type Status = 'pending' | 'accepted' | 'rejected'
-
 interface UserStatusProps {
   className?: string
   name: string
-  status?: Status
 }
 
-export function UserStatus({ name, status = 'pending', className }: UserStatusProps) {
-  let additionalContent
-  let messageContent
+export function UserStatus({ name, className }: UserStatusProps) {
+  const messageContent = `Hi! Thank you for applying ${name} 💚`
+  const additionalContent = (
+    <>
+      <TerminalText>
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">
+          ... coming soon
+        </a>
+      </TerminalText>
 
-  switch (status) {
-    case 'accepted':
-      messageContent = 'WELCOME TO CUHACKING 2025! 💚'
-      additionalContent = (
-        <>
-
-          <TerminalText className="text-orange-400">!!!ACCEPTED!!!</TerminalText>
-          <TerminalText>WE HOPE TO CUHACKING!</TerminalText>
-          <Button
-            className="w-full text-black"
-            type="button"
-            variant="secondary"
-          >
-            RSVP
-          </Button>
-        </>
-      )
-      break
-    case 'pending':
-      messageContent = 'Hi! Thank you for applying'
-      additionalContent = (
-        <div>
-          <TerminalText>
-            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">
-              ... coming soon
-            </a>
-          </TerminalText>
-
-          <TerminalText>
-            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">
-              We hope to cuHacking 👀!!!
-            </a>
-          </TerminalText>
-        </div>
-      )
-      break
-    case 'rejected':
-      additionalContent = (
-        <>
-          <TerminalText>Unfortunately, we couldn't accept your application this time.</TerminalText>
-          <TerminalText>Don't be discouraged! We encourage you to apply again next year. 💪</TerminalText>
-        </>
-      )
-      break
-  }
+      <TerminalText>
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">
+          We hope to cuHacking 👀!!!
+        </a>
+      </TerminalText>
+    </>
+  )
 
   return (
     <GlassmorphicCard className={cn('flex flex-col gap-2 items-start p-3', className)}>
       <Typography variant="paragraph-base">
         {messageContent}
-        {' '}
-        {name}
-        {' '}
-        💚
       </Typography>
       <TerminalText
         className="text-lg text-center"
