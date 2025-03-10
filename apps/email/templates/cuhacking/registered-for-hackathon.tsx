87e955e1213d62468b206a85464f96db20bc1d93
Zesan <zesanrahim@gmail.com>
2025-03-10 16:09:45 -0400

    feat(emails): add new content

--- apps/email/templates/cuhacking/registered-for-hackathon.tsx
+++ apps/email/templates/cuhacking/registered-for-hackathon.tsx
@@ -22,15 +22,45 @@ function getGenericEmailConstants() {
 * } */
   /* catch (error) {
 *   console.error('Error fetching email content:', error) */
-    return {
-      title: 'Your account is created.',
-      body: {
-        buttonText: 'Complete Registration',
-        buttonLink: 'https://portal.cuhacking.ca/terms',
-        text: 'Looking forward to seeing you on March 14th!',
-        footer: 'See you soon!',
+return {
+  title: 'Heeyy :333',
+  body: {
+    text: `Your application to the cuHacking 2025 event at Carleton University has been accepted! 🔥🔥🔥
+
+Details ...
+📍 Richcraft Hall (1125 Colonel By Dr, Carleton University)
+⌛ March 14-16 (Check-in from 5pm - 9pm)
+
+🥗 Food + Merch will be provided for the first 300 hackers on site!
+What to bring?
+🪪 Government ID
+💻 Your laptop + charger
+🛏️ Sleeping bags
+🪥 Personal hygiene (plz bring deodorant, plz ...)
+🥤 Reusable water bottle
+🤪 A positive attitude!`,
+   
+    buttonText: 'JOIN DISCORD',
+    buttonLink: 'https://discord.gg/VnbWdAe8kA',
+
+    secondButtonText: 'ADD TO CALENDAR',
+    secondButtonLink: 'https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=MDY1bnFyb3B2c2Z1NjM4dDRxODcybmhtYWwgZmNkYmEzZjM1NGQ0ZTAxNTUyZTI0OTVkNzQzMTA1YmQ5ZWZjZTRlMTA3NmNkYTNjMWVjMTQ1NGQwZjVmYWE3M0Bn&tmsrc=fcdba3f354d4e01552e2495d743105bd9efce4e1076cda3c1ec1454d0f5faa73%40group.calendar.google.com', 
+
+    events: [
+      {
+        title: 'Upcoming events...',
       },
-    }
+      {
+        title: 'Intro to QNX',
+        text: 'Learn about QNX with 3 awesome speakers + meet some interns. Make sure you bring your laptop. Register now!',
+        buttonText: 'REGISTER',
+      },
+    ],
+  },
+}
+
+
+
   /* } */
 }
 
