00ddfffbdd3ac12aceb103c80e4090da83224b39
Zesan <zesanrahim@gmail.com>
2025-03-10 16:04:41 -0400

    feat(emails): edit email content

--- libs/email/components/content.tsx
+++ libs/email/components/content.tsx
@@ -2,6 +2,8 @@ import { Button, Card } from '@/email'
 import { Text } from '@react-email/components'
 
 export function Content({ title, body }) {
+  const events = body.events || []
+
   return (
     <table className="w-full max-w-[600px] mx-auto bg-black">
       <tbody>
@@ -12,16 +14,31 @@ export function Content({ title, body }) {
                 <tbody>
                   <tr>
                     <td className="text-center">
-                      {body.buttonLink && (
-                        <a href={body.buttonLink}>
-                          <Button>
-                            <Text className="m-0 p-0">{body.buttonText}</Text>
-                          </Button>
-                        </a>
-                      )}
-                      <Text className="text-sm text-[#e6e6e6] font-mono">
+                      <pre className="text-sm text-[#e6e6e6] font-mono text-left">
                         {body.text}
-                      </Text>
+                      </pre>
+                      {body.buttonLink && (
+                        <>
+                          <a href={body.buttonLink} className="block">
+                            <Button>
+                              <Text className="m-0 p-0">
+                                {body.buttonText}
+                              </Text>
+                            </Button>
+                          </a>
+                          <br />
+                          <a
+                            href={body.secondButtonLink || body.buttonText}
+                            className="block"
+                          >
+                            <Button>
+                              <Text className="m-0 p-0">
+                                {body.secondButtonText || body.buttonText}
+                              </Text>
+                            </Button>
+                          </a>
+                        </>
+                      )}
                     </td>
                   </tr>
                 </tbody>
@@ -29,6 +46,56 @@ export function Content({ title, body }) {
             </Card>
           </td>
         </tr>
+        {events.length > 0 && (
+          <tr>
+            <td className="px-10 pt-2">
+              <table className="w-full">
+                <tbody>
+                  <tr>
+                    <td>
+                      <Card title={events[0].title}>
+                        <Text className="text-sm text-[#e6e6e6] font-mono">
+                          {events[0].text}
+                        </Text>
+                        {events[0].buttonText && (
+                          <Button>
+                            <Text className="m-0 p-0">
+                              {events[0].buttonText}
+                            </Text>
+                          </Button>
+                        )}
+                        {events.length > 1 && (
+                          <table className="w-full">
+                            <tbody>
+                              {events.slice(1).map((event, idx) => (
+                                <tr key={idx}>
+                                  <td className="py-4 pl-4">
+                                    <Card title={event.title}>
+                                      <Text className="text-sm text-[#e6e6e6] font-mono">
+                                        {event.text}
+                                      </Text>
+                                      {event.buttonText && (
+                                        <Button>
+                                          <Text className="m-0 p-0">
+                                            {event.buttonText}
+                                          </Text>
+                                        </Button>
+                                      )}
+                                    </Card>
+                                  </td>
+                                </tr>
+                              ))}
+                            </tbody>
+                          </table>
+                        )}
+                      </Card>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </td>
+          </tr>
+        )}
       </tbody>
     </table>
   )
