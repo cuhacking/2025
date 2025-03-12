3bd94fed34d82db7613435d641a63452bb626979
Mumtahin Farabi <mfarabi619@gmail.com>
2025-03-12 00:28:40 -0400

    feat(axiom): redirect immediately to portal from home route rather than authenticating

--- apps/axiom/src/app/(payload)/page.tsx
+++ apps/axiom/src/app/(payload)/page.tsx
@@ -1,32 +1,9 @@
-import { redirect } from 'next/navigation'
-import { cookies } from 'next/headers'
-import { getPayload } from 'payload'
-import config from '@payload-config'
+import { redirect } from 'next/navigation';
 
-export default async function Home() {
-  const payload = await getPayload({ config })
-
-  const token = await cookies()
-
-if (!token.get('payload-token')?.value) {
+export default function Home() {
   return redirect(
     process.env.NODE_ENV === 'development'
       ? process.env.CUHACKING_2025_PORTAL_LOCAL_URL
       : process.env.CUHACKING_2025_PORTAL_PUBLIC_URL
-  )
-}
-
-  try {
-    const { user } = await payload.auth.verifyJWT(token)
-
-    if (user) {
-      redirect('/admin')
-    }
-  } catch (error) {
-    console.error('Auth error:', error)
-    redirect(
-    process.env.NODE_ENV === 'development'
-      ? process.env.CUHACKING_2025_PORTAL_LOCAL_URL
-    : process.env.CUHACKING_2025_PORTAL_PUBLIC_URL)
-  }
+  );
 }
