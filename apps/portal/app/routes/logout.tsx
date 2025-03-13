1b099458291a14db1d05c8ccc47c8122ec5853c4
HasithDeAlwis <hasithde24@gmail.com>
2025-03-13 01:50:39 -0400

    fix(portal): logout redirected to axiom

--- apps/portal/app/routes/logout.tsx
+++ apps/portal/app/routes/logout.tsx
@@ -2,11 +2,37 @@
 import type { LoaderFunction } from '@remix-run/node'
 import { redirect } from '@remix-run/node'
 
-export const loader: LoaderFunction = async () => {
+export const loader: LoaderFunction = async ({ request }) => {
+  const cookie = request.headers.get('Cookie')
   const adminURL
     = process.env.NODE_ENV === 'development'
-      ? `http://localhost:8000/admin/logout`
-      : `https://axiom.cuhacking.ca/admin/logout`
+      ? `http://localhost:8000/api/users/logout`
+      : `https://axiom.cuhacking.ca/api/users/logout`
 
-  return redirect(adminURL)
+  try {
+    const res = await fetch(adminURL, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        'Cookie': cookie || '',
+      },
+    })
+
+    if (!res.ok) {
+      console.error('ERROR - Could not log user out')
+      throw new Error('Failed to log out user')
+    }
+  }
+  catch (error) {
+    console.error('ERROR - An error occurred during logout', error)
+  }
+
+  const response = redirect('/login', {
+    headers: {
+      "Set-Cookie":
+      "payload-token=; path=/"
+    }
+  })
+  return response
 }
