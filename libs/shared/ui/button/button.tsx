c7035d8ae3746de339677b221fbd0ddef21a9720
Mumtahin Farabi <mfarabi619@gmail.com>
2025-03-14 17:27:59 -0400

    fix(ui/portal/schedule): button text unreadable on hover

--- libs/shared/ui/button/button.tsx
+++ libs/shared/ui/button/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          'shadow-buttonKeyboard bg-g-keyboardBlack text-primary hover:bg-primary/90 hover:shadow-buttonKeyboardHover',
+          'shadow-buttonKeyboard bg-g-keyboardBlack text-primary hover:text-background hover:bg-primary/90 hover:shadow-buttonKeyboardHover',
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
